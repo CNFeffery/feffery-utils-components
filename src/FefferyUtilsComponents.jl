2c9fa814194f22e01362c925676150d17b69e9e5
CNFeffery <fefferypzy@gmail.com>
2021-11-08 18:34:49 +0800

    0.0.4开发版本新增FefferySyntaxHighlighter代码高亮功能

--- src/FefferyUtilsComponents.jl
+++ src/FefferyUtilsComponents.jl
@@ -3,7 +3,7 @@ module FefferyUtilsComponents
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "0.0.3"
+const version = "0.0.4"
 
 include("jl/''_fefferycaptcha.jl")
 include("jl/''_fefferysyntaxhighlighter.jl")
@@ -18,14 +18,14 @@ function __init__()
             [
                 DashBase.Resource(
     relative_package_path = "feffery_utils_components.min.js",
-    external_url = "https://unpkg.com/feffery_utils_components@0.0.3/feffery_utils_components/feffery_utils_components.min.js",
+    external_url = "https://unpkg.com/feffery_utils_components@0.0.4/feffery_utils_components/feffery_utils_components.min.js",
     dynamic = nothing,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "feffery_utils_components.min.js.map",
-    external_url = "https://unpkg.com/feffery_utils_components@0.0.3/feffery_utils_components/feffery_utils_components.min.js.map",
+    external_url = "https://unpkg.com/feffery_utils_components@0.0.4/feffery_utils_components/feffery_utils_components.min.js.map",
     dynamic = true,
     async = nothing,
     type = :js
--- src/lib/components/FefferySyntaxHighlighter.react.js
+++ src/lib/components/FefferySyntaxHighlighter.react.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { AiOutlineCheck, AiOutlineCopy } from "react-icons/ai";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import {
     a11yDark,
     atomDark,
@@ -18,36 +20,63 @@ import {
 } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 // 定义代码语法高亮组件FefferySyntaxHighlighter，api参数参考https://github.com/react-syntax-highlighter/react-syntax-highlighter
-export default class FefferySyntaxHighlighter extends Component {
-    render() {
-        // 取得必要属性或参数
-        let {
-            id,
-            codeStyle,
-            codeString,
-            language,
-            showLineNumbers,
-            showInlineLineNumbers,
-            setProps
-        } = this.props;
+const FefferySyntaxHighlighter = (props) => {
+    // 取得必要属性或参数
+    let {
+        id,
+        codeStyle,
+        codeString,
+        language,
+        showLineNumbers,
+        showInlineLineNumbers,
+        setProps
+    } = props;
 
-        const str2style = new Map([
-            ['a11y-dark', a11yDark],
-            ['atom-dark', atomDark],
-            ['coldark-cold', coldarkCold],
-            ['coldark-dark', coldarkDark],
-            ['coy', coy],
-            ['coy-without-shadows', coyWithoutShadows],
-            ['darcula', darcula],
-            ['dracula', dracula],
-            ['nord', nord],
-            ['okaidia', okaidia],
-            ['prism', prism],
-            ['solarizedlight', solarizedlight],
-            ['twilight', twilight]
-        ])
+    const str2style = new Map([
+        ['a11y-dark', a11yDark],
+        ['atom-dark', atomDark],
+        ['coldark-cold', coldarkCold],
+        ['coldark-dark', coldarkDark],
+        ['coy', coy],
+        ['coy-without-shadows', coyWithoutShadows],
+        ['darcula', darcula],
+        ['dracula', dracula],
+        ['nord', nord],
+        ['okaidia', okaidia],
+        ['prism', prism],
+        ['solarizedlight', solarizedlight],
+        ['twilight', twilight]
+    ])
 
-        return (
+    const [isCopied, setIsCopied] = useState(false);
+
+    return (
+        <div style={{ position: 'relative' }}>
+            <CopyToClipboard
+                onCopy={() => {
+                    setIsCopied(true);
+                    setTimeout(() => setIsCopied(false), 3000);
+                }}
+                style={
+                    {
+                        position: 'absolute',
+                        right: '5px',
+                        top: '5px',
+                        padding: '6px',
+                        margin: 0,
+                        background: 'transparent',
+                        border: 'none transparent',
+                        cursor: 'pointer',
+                        zIndex: 999
+                    }
+                }
+                text={codeString}
+            >
+                <button type="button" aria-label="Copy to Clipboard Button">
+                    {isCopied ? <AiOutlineCheck style={{ color: 'rgb(91, 199, 38)', fontSize: '18px' }} />
+                        : <AiOutlineCopy style={{ color: 'rgb(24, 144, 255)', fontSize: '18px' }} />}
+                </button>
+            </CopyToClipboard>
             <SyntaxHighlighter id={id}
                 style={str2style.get(codeStyle)}
                 showLineNumbers={showLineNumbers}
@@ -55,8 +84,8 @@ export default class FefferySyntaxHighlighter extends Component {
                 language={language}>
                 {codeString}
             </SyntaxHighlighter>
-        );
-    }
+        </div>
+    );
 }
 
 // 定义参数或属性
@@ -104,3 +133,5 @@ FefferySyntaxHighlighter.propTypes = {
 FefferySyntaxHighlighter.defaultProps = {
     codeStyle: 'prism'
 }
+
+export default FefferySyntaxHighlighter;
