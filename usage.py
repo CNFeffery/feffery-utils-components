69eaf7d73586828c5a1ad34ddd79cb890f828a9e
CNFeffery <fefferypzy@gmail.com>
2022-01-04 15:38:05 +0800

    清除冗余文件

---
+++ usage.py
@@ -0,0 +1,86 @@
+import feffery_utils_components as fuc
+import dash
+from dash.dependencies import Input, Output
+import dash_html_components as html
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(
+    [
+        fuc.FefferyPasteImage(
+            style={
+                'height': '500px',
+                'width': '800px',
+                'marginBottom': '100px'
+            }
+        ),
+        fuc.FefferyCaptcha(id='captcha-demo',
+                           charNum=10,
+                           width=300,
+                           height=100,
+                           fontSize=60),
+        html.Div(
+            html.Em(id='output-demo')
+        ),
+
+        fuc.FefferyWaterMark(
+            content='fuc FefferyWaterMark',
+            fontSize=26
+        ),
+
+        fuc.FefferySyntaxHighlighter(
+            showLineNumbers=True,
+            showInlineLineNumbers=True,
+            codeString='''html.Div(
+    [
+        fac.AntdBackTop(
+            containerId='back-top-container-demo',
+            duration=1
+        ),
+        fac.AntdTitle(
+            '向下滑动一段距离',
+            level=4
+        )
+    ] + [
+        html.Div(
+            [
+                i if i % 2 == 0 else html.Br() for i in range(200)
+            ]
+        )
+    ],
+    id='back-top-container-demo',
+    style={
+        'maxHeight': '500px',
+        'overflowY': 'auto',
+        'position': 'relative',
+        'backgroundColor': 'rgba(240, 240, 240, 0.2)',
+        'padding': '20px'
+    }
+)
+''',
+            language='python',
+            codeStyle='coy-without-shadows'
+        )
+    ],
+    style={
+        'padding': '100px'
+    }
+)
+
+
+@app.callback(
+    Output('output-demo', 'children'),
+    Input('captcha-demo', 'captcha'),
+    prevent_initial_call=True
+)
+def test(captcha):
+
+    import time
+
+    time.sleep(3)
+
+    return captcha
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
