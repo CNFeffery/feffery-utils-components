befd99ecaa3b3e135ada943dd2cdf9c25873c5f7
CNFeffery <fefferypzy@gmail.com>
2022-02-23 21:37:07 +0800

    更新至0.0.9版本

--- src/FefferyUtilsComponents.jl
+++ src/FefferyUtilsComponents.jl
@@ -3,7 +3,7 @@ module FefferyUtilsComponents
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "0.0.8"
+const version = "0.0.9"
 
 include("jl/''_fefferycaptcha.jl")
 include("jl/''_fefferyexecutejs.jl")
@@ -25,14 +25,14 @@ function __init__()
             [
                 DashBase.Resource(
     relative_package_path = "feffery_utils_components.min.js",
-    external_url = "https://unpkg.com/feffery_utils_components@0.0.8/feffery_utils_components/feffery_utils_components.min.js",
+    external_url = "https://unpkg.com/feffery_utils_components@0.0.9/feffery_utils_components/feffery_utils_components.min.js",
     dynamic = nothing,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "feffery_utils_components.min.js.map",
-    external_url = "https://unpkg.com/feffery_utils_components@0.0.8/feffery_utils_components/feffery_utils_components.min.js.map",
+    external_url = "https://unpkg.com/feffery_utils_components@0.0.9/feffery_utils_components/feffery_utils_components.min.js.map",
     dynamic = true,
     async = nothing,
     type = :js
