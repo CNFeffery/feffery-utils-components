32bae9a9bb85b9bee39208b60fb03b170e3f26e3
CNFeffery <fefferypzy@gmail.com>
2023-09-28 09:51:57 +0800

    fix: 修复FefferyTopProgress无法捕获allow_duplicate=True回调角色的问题 #8

--- src/lib/components/FefferyTopProgress.react.js
+++ src/lib/components/FefferyTopProgress.react.js
@@ -2,106 +2,105 @@ import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import NProgress from 'nprogress';
 import FefferyStyle from './FefferyStyle.react';
-// import './nprogress.css';
 
 
 const parseChildrenToArray = children => {
-  if (children && !Array.isArray(children)) {
-    return [children];
-  }
-  return children;
+    if (children && !Array.isArray(children)) {
+        return [children];
+    }
+    return children;
 };
 
 // 定义顶部加载进度条组件FefferyTopProgress，api参数参考https://github.com/rstacruz/nprogress
 const FefferyTopProgress = (props) => {
-  // 取得必要属性或参数
-  let {
-    id,
-    className,
-    style,
-    children,
-    minimum,
-    easing,
-    speed,
-    showSpinner,
-    spinning,
-    listenPropsMode,
-    excludeProps,
-    includeProps,
-    debug,
-    color,
-    zIndex,
-    setProps,
-    loading_state
-  } = props;
+    // 取得必要属性或参数
+    let {
+        id,
+        className,
+        style,
+        children,
+        minimum,
+        easing,
+        speed,
+        showSpinner,
+        spinning,
+        listenPropsMode,
+        excludeProps,
+        includeProps,
+        debug,
+        color,
+        zIndex,
+        setProps,
+        loading_state
+    } = props;
 
-  // 配置NProgress参数信息
-  NProgress.configure({
-    minimum,
-    easing,
-    speed,
-    showSpinner
-  })
+    // 配置NProgress参数信息
+    NProgress.configure({
+        minimum,
+        easing,
+        speed,
+        showSpinner
+    })
 
-  children = parseChildrenToArray(children)
+    children = parseChildrenToArray(children)
 
-  const [showSpinning, setShowSpinning] = useState(spinning);
-  const timer = useRef();
+    const [showSpinning, setShowSpinning] = useState(spinning);
+    const timer = useRef();
 
-  useEffect(() => {
-    if (loading_state) {
-      if (timer.current) {
-        clearTimeout(timer.current);
-      }
-      if (loading_state.is_loading && !showSpinning) {
-        // 当listenPropsMode为'default'时
-        if (listenPropsMode === 'default') {
-          if (debug) {
-            console.log(loading_state.component_name + '.' + loading_state.prop_name)
-          }
-          setShowSpinning(true);
-          NProgress.start();
-        } else if (listenPropsMode === 'exclude') {
-          // 当listenPropsMode为'exclude'模式时
-          // 当前触发loading_state的组件+属性组合不在排除列表中时，激活动画
-          if (excludeProps.indexOf(loading_state.component_name + '.' + loading_state.prop_name) === -1) {
-            if (debug) {
-              console.log(loading_state.component_name + '.' + loading_state.prop_name)
+    useEffect(() => {
+        if (loading_state) {
+            if (timer.current) {
+                clearTimeout(timer.current);
             }
-            setShowSpinning(true);
-            NProgress.start();
-          }
-        } else if (listenPropsMode === 'include') {
-          // 当listenPropsMode为'include'模式时
-          // 当前触发loading_state的组件+属性组合在包含列表中时，激活动画
-          if (includeProps.indexOf(loading_state.component_name + '.' + loading_state.prop_name) !== -1) {
-            if (debug) {
-              console.log(loading_state.component_name + '.' + loading_state.prop_name)
+            if (loading_state.is_loading && !showSpinning) {
+                // 当listenPropsMode为'default'时
+                if (listenPropsMode === 'default') {
+                    if (debug) {
+                        console.log(loading_state.component_name + '.' + loading_state.prop_name.split('@')[0])
+                    }
+                    setShowSpinning(true);
+                    NProgress.start();
+                } else if (listenPropsMode === 'exclude') {
+                    // 当listenPropsMode为'exclude'模式时
+                    // 当前触发loading_state的组件+属性组合不在排除列表中时，激活动画
+                    if (excludeProps.indexOf(loading_state.component_name + '.' + loading_state.prop_name.split('@')[0]) === -1) {
+                        if (debug) {
+                            console.log(loading_state.component_name + '.' + loading_state.prop_name.split('@')[0])
+                        }
+                        setShowSpinning(true);
+                        NProgress.start();
+                    }
+                } else if (listenPropsMode === 'include') {
+                    // 当listenPropsMode为'include'模式时
+                    // 当前触发loading_state的组件+属性组合在包含列表中时，激活动画
+                    if (includeProps.indexOf(loading_state.component_name + '.' + loading_state.prop_name.split('@')[0]) !== -1) {
+                        if (debug) {
+                            console.log(loading_state.component_name + '.' + loading_state.prop_name.split('@')[0])
+                        }
+                        setShowSpinning(true);
+                        NProgress.start();
+                    }
+                }
+
+            } else if (!loading_state.is_loading && showSpinning) {
+                timer.current = setTimeout(() => {
+                    setShowSpinning(false);
+                    NProgress.done();
+                });
             }
-            setShowSpinning(true);
-            NProgress.start();
-          }
         }
+    }, [loading_state]);
 
-      } else if (!loading_state.is_loading && showSpinning) {
-        timer.current = setTimeout(() => {
-          setShowSpinning(false);
-          NProgress.done();
-        });
-      }
-    }
-  }, [loading_state]);
-
-  // 返回定制化的前端组件
-  return (<div id={id}
-    className={className}
-    style={style}
-    data-dash-is-loading={
-      (loading_state && loading_state.is_loading) || undefined
-    } >
-    <FefferyStyle
-      rawStyle={
-        `
+    // 返回定制化的前端组件
+    return (<div id={id}
+        className={className}
+        style={style}
+        data-dash-is-loading={
+            (loading_state && loading_state.is_loading) || undefined
+        } >
+        <FefferyStyle
+            rawStyle={
+                `
 #nprogress {
   pointer-events: none;
 }
@@ -186,97 +185,97 @@ const FefferyTopProgress = (props) => {
   }
 }
 `
-      }
-    />
-    {children}
-  </div>
-  );
+            }
+        />
+        {children}
+    </div>
+    );
 }
 
 FefferyTopProgress._dashprivate_isLoadingComponent = true;
 
 // 定义参数或属性
 FefferyTopProgress.propTypes = {
-  // 组件id
-  id: PropTypes.string,
+    // 组件id
+    id: PropTypes.string,
 
-  /**
-   * The content of the tab - will only be displayed if this tab is selected
-   */
-  children: PropTypes.node,
-
-  // css类名
-  className: PropTypes.string,
-
-  // 自定义css字典
-  style: PropTypes.object,
-
-  // 设置是否处于加载中状态
-  spinning: PropTypes.bool,
-
-  // 设置顶端进度条的初始进度值，默认为0.33，取值在0到1之间
-  minimum: PropTypes.number,
-
-  // 用于设置同名css动画效果，默认为'ease'
-  easing: PropTypes.string,
-
-  // 设置进度条每步递增耗时（单位：毫秒），默认为200
-  speed: PropTypes.number,
-
-  // 设置是否渲染右上角圆圈加载动画，默认为true
-  showSpinner: PropTypes.bool,
-
-  // 设置是否开启debug模式，开启后，每次动画加载都会在开发者工具的控制台打印prop信息
-  debug: PropTypes.bool,
-
-  // 设置自定义监听组件的模式，可选的有'default'、'exclude'、'include'，默认为'default'
-  listenPropsMode: PropTypes.oneOf(['default', 'exclude', 'include']),
-
-  // 设置需要忽略输出监听过程的组件信息列表
-  // 仅在listenPropsMode为'exclude'时生效
-  excludeProps: PropTypes.arrayOf(PropTypes.string),
-
-  // 设置需要包含输出监听过程的组件信息列表
-  // 仅在listenPropsMode为'include'时生效
-  includeProps: PropTypes.arrayOf(PropTypes.string),
-
-  // 设置顶部进度条色彩，默认为'#29d'
-  color: PropTypes.string,
-
-  // 设置顶部进度条的z-index，默认为99999
-  zIndex: PropTypes.number,
-
-  loading_state: PropTypes.shape({
     /**
-     * Determines if the component is loading or not
+     * The content of the tab - will only be displayed if this tab is selected
      */
-    is_loading: PropTypes.bool,
-    /**
-     * Holds which property is loading
-     */
-    prop_name: PropTypes.string,
-    /**
-     * Holds the name of the component that is loading
-     */
-    component_name: PropTypes.string
-  }),
+    children: PropTypes.node,
 
-  /**
-   * Dash-assigned callback that should be called to report property changes
-   * to Dash, to make them available for callbacks.
-   */
-  setProps: PropTypes.func
+    // css类名
+    className: PropTypes.string,
+
+    // 自定义css字典
+    style: PropTypes.object,
+
+    // 设置是否处于加载中状态
+    spinning: PropTypes.bool,
+
+    // 设置顶端进度条的初始进度值，默认为0.33，取值在0到1之间
+    minimum: PropTypes.number,
+
+    // 用于设置同名css动画效果，默认为'ease'
+    easing: PropTypes.string,
+
+    // 设置进度条每步递增耗时（单位：毫秒），默认为200
+    speed: PropTypes.number,
+
+    // 设置是否渲染右上角圆圈加载动画，默认为true
+    showSpinner: PropTypes.bool,
+
+    // 设置是否开启debug模式，开启后，每次动画加载都会在开发者工具的控制台打印prop信息
+    debug: PropTypes.bool,
+
+    // 设置自定义监听组件的模式，可选的有'default'、'exclude'、'include'，默认为'default'
+    listenPropsMode: PropTypes.oneOf(['default', 'exclude', 'include']),
+
+    // 设置需要忽略输出监听过程的组件信息列表
+    // 仅在listenPropsMode为'exclude'时生效
+    excludeProps: PropTypes.arrayOf(PropTypes.string),
+
+    // 设置需要包含输出监听过程的组件信息列表
+    // 仅在listenPropsMode为'include'时生效
+    includeProps: PropTypes.arrayOf(PropTypes.string),
+
+    // 设置顶部进度条色彩，默认为'#29d'
+    color: PropTypes.string,
+
+    // 设置顶部进度条的z-index，默认为99999
+    zIndex: PropTypes.number,
+
+    loading_state: PropTypes.shape({
+        /**
+         * Determines if the component is loading or not
+         */
+        is_loading: PropTypes.bool,
+        /**
+         * Holds which property is loading
+         */
+        prop_name: PropTypes.string,
+        /**
+         * Holds the name of the component that is loading
+         */
+        component_name: PropTypes.string
+    }),
+
+    /**
+     * Dash-assigned callback that should be called to report property changes
+     * to Dash, to make them available for callbacks.
+     */
+    setProps: PropTypes.func
 };
 
 // 设置默认参数
 FefferyTopProgress.defaultProps = {
-  spinning: false,
-  listenPropsMode: 'default',
-  excludeProps: [],
-  includeProps: [],
-  debug: false,
-  color: '#29d',
-  zIndex: 99999
+    spinning: false,
+    listenPropsMode: 'default',
+    excludeProps: [],
+    includeProps: [],
+    debug: false,
+    color: '#29d',
+    zIndex: 99999
 }
 
 export default FefferyTopProgress;
--- usage.py
+++ usage.py
@@ -1,25 +1,59 @@
 import dash
-import json
+import time
 from dash import html
-import feffery_utils_components.alias as fuc
+import feffery_utils_components as fuc
 from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div(
-    [
-        fuc.Div(
-            # wheelEventStrategy='internally-only',
-            style={
-                'height': 400,
-                'background': '#f0f0f0'
-            }
-        ),
-    ],
-    style={
-        'padding': '50px 50px 10000px 50px'
-    }
+app.layout = fuc.FefferyTopProgress(
+    html.Div(
+        [
+            html.Button(
+                '测试1',
+                id='button1'
+            ),
+            html.Button(
+                '测试2',
+                id='button2'
+            ),
+            html.Div(
+                id='demo-output'
+            )
+        ],
+        style={
+            'padding': 50
+        }
+    ),
+    listenPropsMode='include',
+    includeProps=['demo-output.children'],
+    debug=True
 )
+
+
+@app.callback(
+    Output('demo-output', 'children', allow_duplicate=True),
+    Input('button1', 'n_clicks'),
+    prevent_initial_call=True
+)
+def demo1(n_clicks):
+
+    time.sleep(2)
+
+    return '测试1：%s' % n_clicks
+
+
+@app.callback(
+    Output('demo-output', 'children', allow_duplicate=True),
+    Input('button2', 'n_clicks'),
+    prevent_initial_call=True
+)
+def demo2(n_clicks):
+
+    time.sleep(2)
+
+    return '测试2：%s' % n_clicks
+
 
 if __name__ == '__main__':
     app.run(debug=True)
