a82a8a89cd2c94725f69e3e9f8c2f9c0f35aad4e
insistence <3055204202@qq.com>
2023-12-01 16:24:59 +0800

    perf: FefferyMusicPlayer静态资源异步化

--- feffery_utils_components/__init__.py
+++ feffery_utils_components/__init__.py
@@ -28,7 +28,7 @@ _current_path = _os.path.dirname(_os.path.abspath(__file__))
 
 _this_module = _sys.modules[__name__]
 
-async_resources = ['captcha', 'aplayer']
+async_resources = ['captcha', 'aplayer', 'music_player']
 
 _js_dist = []
 
