5ef49e53146acab0853db8886c35ee1b04f03686
CNFeffery <fefferypzy@gmail.com>
2023-12-07 13:06:04 +0800

    chore: 更新alias别名模块

--- feffery_utils_components/alias.py
+++ feffery_utils_components/alias.py
@@ -1,77 +1,78 @@
-from .FefferyAutoAnimate import FefferyAutoAnimate as AutoAnimate
-from .FefferyMotion import FefferyMotion as Motion
-from .FefferyBlockColorPicker import FefferyBlockColorPicker as BlockColorPicker
-from .FefferyCircleColorPicker import FefferyCircleColorPicker as CircleColorPicker
-from .FefferyEyeDropper import FefferyEyeDropper as EyeDropper
-from .FefferyGithubColorPicker import FefferyGithubColorPicker as GithubColorPicker
-from .FefferyHexColorPicker import FefferyHexColorPicker as HexColorPicker
-from .FefferyRgbColorPicker import FefferyRgbColorPicker as RgbColorPicker
-from .FefferyTwitterColorPicker import FefferyTwitterColorPicker as TwitterColorPicker
-from .FefferyWheelColorPicker import FefferyWheelColorPicker as WheelColorPicker
-from .FefferyGrid import FefferyGrid as Grid
-from .FefferyGridItem import FefferyGridItem as GridItem
-from .FefferyCaptcha import FefferyCaptcha as Captcha
-from .FefferyCompareSlider import FefferyCompareSlider as CompareSlider
-from .FefferyCountDown import FefferyCountDown as CountDown
-from .FefferyCountUp import FefferyCountUp as CountUp
-from .FefferyCssVar import FefferyCssVar as CssVar
-from .FefferyDiv import FefferyDiv as Div
-from .FefferyExecuteJs import FefferyExecuteJs as ExecuteJs
-from .FefferyExternalCss import FefferyExternalCss as ExternalCss
-from .FefferyExternalJs import FefferyExternalJs as ExternalJs
-from .FefferyExtraSpinner import FefferyExtraSpinner as ExtraSpinner
-from .FefferyFancyButton import FefferyFancyButton as FancyButton
-from .FefferyFancyMessage import FefferyFancyMessage as FancyMessage
-from .FefferyFancyNotification import FefferyFancyNotification as FancyNotification
-from .FefferyFullscreen import FefferyFullscreen as Fullscreen
-from .FefferyGuide import FefferyGuide as Guide
-from .FefferyHighlightWords import FefferyHighlightWords as HighlightWords
-from .FefferyImagePaste import FefferyImagePaste as ImagePaste
-from .FefferyJsonViewer import FefferyJsonViewer as JsonViewer
-from .FefferyLazyLoad import FefferyLazyLoad as LazyLoad
-from .FefferyQRCode import FefferyQRCode as QRCode
-from .FefferyRawHTML import FefferyRawHTML as RawHTML
-from .FefferyReload import FefferyReload as Reload
-from .FefferyScroll import FefferyScroll as Scroll
-from .FefferyScrollbars import FefferyScrollbars as Scrollbars
-from .FefferySetTitle import FefferySetTitle as SetTitle
-from .FefferyShadowDom import FefferyShadowDom as ShadowDom
-from .FefferyShortcutPanel import FefferyShortcutPanel as ShortcutPanel
-from .FefferySticky import FefferySticky as Sticky
-from .FefferyStyle import FefferyStyle as Style
-from .FefferyTabMessenger import FefferyTabMessenger as TabMessenger
-from .FefferyTimeout import FefferyTimeout as Timeout
-from .FefferyTopProgress import FefferyTopProgress as TopProgress
-from .FefferyVirtualList import FefferyVirtualList as VirtualList
-from .FefferyWebSocket import FefferyWebSocket as WebSocket
-from .FefferyDeviceDetect import FefferyDeviceDetect as DeviceDetect
-from .FefferyDocumentVisibility import FefferyDocumentVisibility as DocumentVisibility
-from .FefferyGeolocation import FefferyGeolocation as Geolocation
-from .FefferyIdle import FefferyIdle as Idle
-from .FefferyInViewport import FefferyInViewport as InViewport
-from .FefferyKeyPress import FefferyKeyPress as KeyPress
-from .FefferyListenPaste import FefferyListenPaste as ListenPaste
-from .FefferyListenScroll import FefferyListenScroll as ListenScroll
-from .FefferyListenUnload import FefferyListenUnload as ListenUnload
-from .FefferyLocation import FefferyLocation as Location
-from .FefferyLongPress import FefferyLongPress as LongPress
-from .FefferyMousePosition import FefferyMousePosition as MousePosition
-from .FefferyResponsive import FefferyResponsive as Responsive
-from .FefferyTextSelection import FefferyTextSelection as TextSelection
-from .FefferyWindowSize import FefferyWindowSize as WindowSize
-from .FefferyAPlayer import FefferyAPlayer as APlayer
-from .FefferyDPlayer import FefferyDPlayer as DPlayer
-from .FefferyMusicPlayer import FefferyMusicPlayer as MusicPlayer
-from .FefferyResizable import FefferyResizable as Resizable
-from .FefferySortableList import FefferySortableList as SortableList
-from .FefferySplit import FefferySplit as Split
-from .FefferySplitPane import FefferySplitPane as SplitPane
-from .FefferyCookie import FefferyCookie as Cookie
-from .FefferyDebounceProp import FefferyDebounceProp as DebounceProp
-from .FefferyLocalLargeStorage import FefferyLocalLargeStorage as LocalLargeStorage
-from .FefferyLocalStorage import FefferyLocalStorage as LocalStorage
-from .FefferySessionStorage import FefferySessionStorage as SessionStorage
-from .FefferyThrottleProp import FefferyThrottleProp as ThrottleProp
+from .FefferyAutoAnimate import AutoAnimate
+from .FefferyMotion import Motion
+from .FefferyBlockColorPicker import BlockColorPicker
+from .FefferyCircleColorPicker import CircleColorPicker
+from .FefferyEyeDropper import EyeDropper
+from .FefferyGithubColorPicker import GithubColorPicker
+from .FefferyHexColorPicker import HexColorPicker
+from .FefferyRgbColorPicker import RgbColorPicker
+from .FefferyTwitterColorPicker import TwitterColorPicker
+from .FefferyWheelColorPicker import WheelColorPicker
+from .FefferyGrid import Grid
+from .FefferyGridItem import GridItem
+from .FefferyCaptcha import Captcha
+from .FefferyCompareSlider import CompareSlider
+from .FefferyCountDown import CountDown
+from .FefferyCountUp import CountUp
+from .FefferyCssVar import CssVar
+from .FefferyDiv import Div
+from .FefferyEmojiPicker import EmojiPicker
+from .FefferyExecuteJs import ExecuteJs
+from .FefferyExternalCss import ExternalCss
+from .FefferyExternalJs import ExternalJs
+from .FefferyExtraSpinner import ExtraSpinner
+from .FefferyFancyButton import FancyButton
+from .FefferyFancyMessage import FancyMessage
+from .FefferyFancyNotification import FancyNotification
+from .FefferyFullscreen import Fullscreen
+from .FefferyGuide import Guide
+from .FefferyHighlightWords import HighlightWords
+from .FefferyImagePaste import ImagePaste
+from .FefferyJsonViewer import JsonViewer
+from .FefferyLazyLoad import LazyLoad
+from .FefferyQRCode import QRCode
+from .FefferyRawHTML import RawHTML
+from .FefferyReload import Reload
+from .FefferyScroll import Scroll
+from .FefferyScrollbars import Scrollbars
+from .FefferySetTitle import SetTitle
+from .FefferyShadowDom import ShadowDom
+from .FefferyShortcutPanel import ShortcutPanel
+from .FefferySticky import Sticky
+from .FefferyStyle import Style
+from .FefferyTabMessenger import TabMessenger
+from .FefferyTimeout import Timeout
+from .FefferyTopProgress import TopProgress
+from .FefferyVirtualList import VirtualList
+from .FefferyWebSocket import WebSocket
+from .FefferyDeviceDetect import DeviceDetect
+from .FefferyDocumentVisibility import DocumentVisibility
+from .FefferyGeolocation import Geolocation
+from .FefferyIdle import Idle
+from .FefferyInViewport import InViewport
+from .FefferyKeyPress import KeyPress
+from .FefferyListenPaste import ListenPaste
+from .FefferyListenScroll import ListenScroll
+from .FefferyListenUnload import ListenUnload
+from .FefferyLocation import Location
+from .FefferyLongPress import LongPress
+from .FefferyMousePosition import MousePosition
+from .FefferyResponsive import Responsive
+from .FefferyTextSelection import TextSelection
+from .FefferyWindowSize import WindowSize
+from .FefferyAPlayer import APlayer
+from .FefferyDPlayer import DPlayer
+from .FefferyMusicPlayer import MusicPlayer
+from .FefferyResizable import Resizable
+from .FefferySortableList import SortableList
+from .FefferySplit import Split
+from .FefferySplitPane import SplitPane
+from .FefferyCookie import Cookie
+from .FefferyDebounceProp import DebounceProp
+from .FefferyLocalLargeStorage import LocalLargeStorage
+from .FefferyLocalStorage import LocalStorage
+from .FefferySessionStorage import SessionStorage
+from .FefferyThrottleProp import ThrottleProp
 
 __all__ = [
     "AutoAnimate",
@@ -92,6 +93,7 @@ __all__ = [
     "CountUp",
     "CssVar",
     "Div",
+    "EmojiPicker",
     "ExecuteJs",
     "ExternalCss",
     "ExternalJs",
