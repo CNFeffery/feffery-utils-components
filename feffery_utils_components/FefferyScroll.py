c2971354192e6eeacc99f2c89f4fe5a152f2c1cd
insistence <3055204202@qq.com>
2023-12-09 21:07:27 +0800

    doc: 重构FefferyScroll参数注释

--- feffery_utils_components/FefferyScroll.py
+++ feffery_utils_components/FefferyScroll.py
@@ -9,15 +9,21 @@ class FefferyScroll(Component):
 
 Keyword arguments:
 
-- id (string; optional)
+- id (string; optional):
+    组件id.
 
-- containerId (string; optional)
+- containerId (string; optional):
+    当滚动目标位于局部滚动条内时，用于设置局部滚动条所在的容器id信息*.
 
-- delay (number; default 0)
+- delay (number; default 0):
+    用于设置滚动延时（单位：毫秒），默认为0.
 
-- duration (number; default 500)
+- duration (number; default 500):
+    用于设置滚动过程耗时（单位：毫秒），默认为500.
 
-- executeScroll (boolean; default False)
+- executeScroll (boolean; default False):
+    用于指示是否进行滚动操作，默认为False  在回调中可executeScroll参数Output为True从而触发新一次滚动
+    每次由executeScroll=True触发的滚动完成后，executeScroll会自动重置为False.
 
 - loading_state (dict; optional)
 
@@ -32,17 +38,23 @@ Keyword arguments:
     - prop_name (string; optional):
         Holds which property is loading.
 
-- offset (number; optional)
+- offset (number; optional):
+    设置滚动过程的额外偏移像素距离.
 
-- scrollMode (a value equal to: 'to-top', 'to-bottom', 'top-offset', 'relative-offset', 'target'; default 'to-top')
+- scrollMode (a value equal to: 'to-top', 'to-bottom', 'top-offset', 'relative-offset', 'target'; default 'to-top'):
+    设置页面滚动模式，可选的有'top'、'bottom'、'top-offset'、'relative-offset'、'target'.
 
-- scrollRelativeOffset (number; optional)
+- scrollRelativeOffset (number; optional):
+    当scrollMode='relative-offset'时，用于设置相对滚动的像素距离，负数则为向上滚动.
 
-- scrollTargetId (string; optional)
+- scrollTargetId (string; optional):
+    当scrollMode='target'时，用于设置滚动目标元素的id信息.
 
-- scrollTopOffset (number; optional)
+- scrollTopOffset (number; optional):
+    当scrollMode='top-offset'时，用于设置滚动终点距离页面顶端的像素.
 
-- smooth (boolean | a value equal to: 'linear', 'easeInQuad', 'easeOutQuad', 'easeInOutQuad', 'easeInCubic', 'easeOutCubic', 'easeInOutCubic', 'easeInQuart', 'easeOutQuart', 'easeInOutQuart', 'easeInQuint', 'easeOutQuint', 'easeInOutQuint'; default True)"""
+- smooth (boolean | a value equal to: 'linear', 'easeInQuad', 'easeOutQuad', 'easeInOutQuad', 'easeInCubic', 'easeOutCubic', 'easeInOutCubic', 'easeInQuart', 'easeOutQuart', 'easeInOutQuart', 'easeInQuint', 'easeOutQuint', 'easeInOutQuint'; default True):
+    用于设置滚动过程动画模式，默认为True."""
     _children_props = []
     _base_nodes = ['children']
     _namespace = 'feffery_utils_components'
--- usage.py
+++ usage.py
@@ -9,14 +9,51 @@ app = dash.Dash(__name__, compress=True)
 
 app.layout = html.Div(
     [
-        fuc.FefferyFancyButton(
-            '立即重载',
-            id='trigger-reload-demo',
-            type='primary'
+        html.Div(
+            [
+                fuc.FefferyFancyButton(
+                    '滚动至页面底部',
+                    id='scroll-to-bottom-demo',
+                    type='primary'
+                ),
+                fuc.FefferyFancyButton(
+                    '滚动至距离顶部800px处',
+                    id='scroll-top-offset-demo',
+                    type='primary'
+                ),
+                fuc.FefferyFancyButton(
+                    '相对当前位置向下滚动300px',
+                    id='scroll-relative-offset-demo',
+                    type='primary'
+                ),
+                fuc.FefferyFancyButton(
+                    '滚动至示例元素处',
+                    id='scroll-target-demo',
+                    type='primary'
+                )
+            ]
         ),
-        fuc.FefferyReload(
-            id='trigger-reload-demo-output'
+        html.Div(
+            [
+                html.Div(
+                    '示例目标元素',
+                    id='scroll-target-element'
+                ),
+                fuc.FefferyFancyButton(
+                    '滚动至页面顶部',
+                    id='scroll-to-top-demo',
+                    type='primary'
+                ),
+            ],
+            style={
+                'padding': '1400px 0 800px 0',
+                'background': 'linear-gradient(180deg,#1890ff80,#fff)'
+            }
         ),
+
+        html.Div(
+            id='scroll-demo-output'
+        )
     ],
     style={
         'padding': 25
@@ -25,13 +62,51 @@ app.layout = html.Div(
 
 
 @app.callback(
-    Output('trigger-reload-demo-output', 'reload'),
-    Input('trigger-reload-demo', 'nClicks'),
+    Output('scroll-demo-output', 'children'),
+    [Input('scroll-to-top-demo', 'nClicks'),
+     Input('scroll-to-bottom-demo', 'nClicks'),
+     Input('scroll-top-offset-demo', 'nClicks'),
+     Input('scroll-relative-offset-demo', 'nClicks'),
+     Input('scroll-target-demo', 'nClicks')],
     prevent_initial_call=True
 )
-def reload_demo(nClicks):
+def scroll_demo(*args):
 
-    return True
+    # 基于dash的上下文功能获知当前回调由谁触发
+    trigger_id = dash.ctx.triggered_id
+
+    if trigger_id == 'scroll-to-top-demo':
+        return fuc.FefferyScroll(
+            executeScroll=True,
+            scrollMode='to-top'
+        )
+
+    elif trigger_id == 'scroll-to-bottom-demo':
+        return fuc.FefferyScroll(
+            executeScroll=True,
+            scrollMode='to-bottom'
+        )
+
+    elif trigger_id == 'scroll-top-offset-demo':
+        return fuc.FefferyScroll(
+            executeScroll=True,
+            scrollMode='top-offset',
+            scrollTopOffset=800
+        )
+
+    elif trigger_id == 'scroll-relative-offset-demo':
+        return fuc.FefferyScroll(
+            executeScroll=True,
+            scrollMode='relative-offset',
+            scrollRelativeOffset=300
+        )
+
+    elif trigger_id == 'scroll-target-demo':
+        return fuc.FefferyScroll(
+            executeScroll=True,
+            scrollMode='target',
+            scrollTargetId='scroll-target-element'
+        )
 
 
 if __name__ == '__main__':
