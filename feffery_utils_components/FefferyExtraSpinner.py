aa9e3958855446a85da92d9f5f9047cbf5949a1c
insistence <3055204202@qq.com>
2023-12-09 20:13:13 +0800

    doc: 重构FefferyExtraSpinner参数注释

--- feffery_utils_components/FefferyExtraSpinner.py
+++ feffery_utils_components/FefferyExtraSpinner.py
@@ -15,13 +15,17 @@ Keyword arguments:
 - backColor (string; default '#1890ff'):
     设置背景色.
 
-- className (string; optional)
+- className (string; optional):
+    css类名.
 
 - color (string; default '#1890ff'):
     设置颜色.
 
 - frontColor (string; default '#def6ff'):
     设置前景色.
+
+- key (string; optional):
+    辅助刷新用唯一标识key值.
 
 - loading_state (dict; optional)
 
@@ -42,7 +46,8 @@ Keyword arguments:
 - sizeUnit (string; default 'px'):
     设置尺寸值单位，默认为'px'.
 
-- style (dict; optional)
+- style (dict; optional):
+    自定义css字典.
 
 - type (a value equal to: "ball", "swap", "bars", "grid", "wave", "push", "firework", "stage", "ring", "heart", "guard", "rotate", "spiral", "pulse", "swish", "sequence", "impulse", "cube", "magic", "flag", "fill", "sphere", "domino", "goo", "comb", "pong", "rainbow", "hoop", "flapper", "jellyfish", "trace", "classic", "whisper", "metro"; default 'ball'):
     加载动画类型."""
@@ -51,10 +56,10 @@ Keyword arguments:
     _namespace = 'feffery_utils_components'
     _type = 'FefferyExtraSpinner'
     @_explicitize_args
-    def __init__(self, id=Component.UNDEFINED, style=Component.UNDEFINED, className=Component.UNDEFINED, type=Component.UNDEFINED, size=Component.UNDEFINED, sizeUnit=Component.UNDEFINED, color=Component.UNDEFINED, frontColor=Component.UNDEFINED, backColor=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['id', 'backColor', 'className', 'color', 'frontColor', 'loading_state', 'size', 'sizeUnit', 'style', 'type']
+    def __init__(self, id=Component.UNDEFINED, style=Component.UNDEFINED, className=Component.UNDEFINED, key=Component.UNDEFINED, type=Component.UNDEFINED, size=Component.UNDEFINED, sizeUnit=Component.UNDEFINED, color=Component.UNDEFINED, frontColor=Component.UNDEFINED, backColor=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'backColor', 'className', 'color', 'frontColor', 'key', 'loading_state', 'size', 'sizeUnit', 'style', 'type']
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['id', 'backColor', 'className', 'color', 'frontColor', 'loading_state', 'size', 'sizeUnit', 'style', 'type']
+        self.available_properties = ['id', 'backColor', 'className', 'color', 'frontColor', 'key', 'loading_state', 'size', 'sizeUnit', 'style', 'type']
         self.available_wildcard_properties =            []
         _explicit_args = kwargs.pop('_explicit_args')
         _locals = locals()
