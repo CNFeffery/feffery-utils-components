8c332be67dc0736a1908e66a6533143cbf1c1e26
insistence <3055204202@qq.com>
2023-12-09 21:35:00 +0800

    doc: 重构FefferySticky参数注释

--- src/lib/components/FefferySticky.react.js
+++ src/lib/components/FefferySticky.react.js
@@ -7,6 +7,7 @@ const FefferySticky = (props) => {
     // 取得必要属性或参数
     const {
         id,
+        key,
         children,
         enabled,
         top,
@@ -18,6 +19,7 @@ const FefferySticky = (props) => {
 
     return <Sticky
         id={id}
+        key={key}
         enabled={enabled}
         top={top}
         bottomBoundary={bottomBoundary}
@@ -32,33 +34,51 @@ const FefferySticky = (props) => {
 
 // 定义参数或属性
 FefferySticky.propTypes = {
-    // 组件id
+    /**
+     * 组件id
+     */
     id: PropTypes.string,
 
+    /**
+     * 辅助刷新用唯一标识key值
+     */
+    key: PropTypes.string,
+
+    /**
+     * 组件子元素
+     */
     children: PropTypes.node,
 
-    // 设置是否启用粘性布局效果，默认为true
+    /**
+     * 设置是否启用粘性布局效果，默认为true
+     */
     enabled: PropTypes.bool,
 
-    // 用于设置触发粘性布局效果对应的顶部距离阈值
-    // 当传入字符型时，字符应为css选择器规则，则此时的顶部距离阈值
-    // 默认为0
-    // 为选择器对应目标元素的高度
+    /**
+     * 用于设置触发粘性布局效果对应的顶部距离阈值，
+     * 当传入字符型时，字符应为css选择器规则，则此时的顶部距离阈值
+     * 默认为0，
+     * 为选择器对应目标元素的高度
+     */
     top: PropTypes.oneOfType([
         PropTypes.number,
         PropTypes.string
     ]),
 
-    // 设置粘性布局元素底部距离文档顶部的像素距离阈值，
-    // 当超出这个阈值范围时，粘性状态会被解除
-    // 当传入字符型时，字符应为css选择器规则，则此时当目标元素底部
-    // 到达粘性布局元素底部时，粘性状态会被解除
+    /**
+     * 设置粘性布局元素底部距离文档顶部的像素距离阈值，
+     * 当超出这个阈值范围时，粘性状态会被解除；
+     * 当传入字符型时，字符应为css选择器规则，则此时当目标元素底部
+     * 到达粘性布局元素底部时，粘性状态会被解除
+     */
     bottomBoundary: PropTypes.oneOfType([
         PropTypes.number,
         PropTypes.string
     ]),
 
-    // 设置粘性布局元素的z-index属性
+    /**
+     * 设置粘性布局元素的z-index属性
+     */
     zIndex: PropTypes.number,
 
     loading_state: PropTypes.shape({
--- usage.py
+++ usage.py
@@ -9,48 +9,25 @@ app = dash.Dash(__name__, compress=True)
 
 app.layout = html.Div(
     [
-        fuc.FefferyShadowDom(
-            [
-                fuc.FefferyStyle(
-                    rawStyle='''
-.style-demo {
-    width: 300px;
-    height: 150px;
-    background: linear-gradient(135deg,#fce38a,#f38181);
-    border-radius: 5px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-'''
-                ),
-                html.Div(
-                    '节点1',
-                    className='style-demo'
-                )
-            ]
+        html.Div(
+            style={
+                'height': '200px'
+            }
         ),
-
-        fuc.FefferyShadowDom(
-            [
-                fuc.FefferyStyle(
-                    rawStyle='''
-.style-demo {
-    width: 300px;
-    height: 150px;
-    background: linear-gradient(135deg,#17ead9,#6078ea);
-    border-radius: 5px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-'''
-                ),
-                html.Div(
-                    '节点2',
-                    className='style-demo'
-                )
-            ]
+        fuc.FefferySticky(
+            html.Div(
+                '请向下滚动查看效果',
+                style={
+                    'backgroundColor': 'green'
+                }
+            ),
+            top=25,
+            bottomBoundary=800
+        ),
+        html.Div(
+            style={
+                'height': '1200px'
+            }
         )
     ],
     style={
