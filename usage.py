3f0e2797b32f28f42b6c0e4bcfe9c80b25deeb30
insistence <3055204202@qq.com>
2023-12-09 21:01:03 +0800

    doc: 重构FefferyLazyLoad参数注释

--- usage.py
+++ usage.py
@@ -3,52 +3,46 @@ import dash
 from dash import html
 import feffery_utils_components as fuc
 from dash.dependencies import Input, Output
+import uuid
 
 app = dash.Dash(__name__, compress=True)
 
 app.layout = html.Div(
     [
-        fuc.FefferyDiv(
-            '鼠标移入此区域后进行图片粘贴',
-            id='image-paste-container',
-            shadow='hover-shadow',
+
+        html.Div(
+            [
+                html.Div(
+                    style={
+                        'height': '800px'
+                    }
+                ),
+                fuc.FefferyLazyLoad(
+                    html.Div(
+                        html.Img(
+                            src='https://fac.feffery.tech/assets/imgs/fac-logo.svg?token=' +
+                            str(uuid.uuid4()),
+                            style={
+                                'height': '100%'
+                            }
+                        ),
+                        style={
+                            'height': '100px',
+                            'border': '1px dashed #c8c6c4'
+                        }
+                    ),
+                    height=100
+                )
+            ],
             style={
-                'height': '200px',
-                'display': 'flex',
-                'justifyContent': 'center',
-                'alignItems': 'center',
-                'borderRadius': '6px',
-                'border': '1px solid #f0f0f0',
-                'marginBottom': '10px'
+                'maxHeight': '300px',
+                'overflow': 'auto'
             }
-        ),
-
-        fuc.FefferyImagePaste(
-            id='image-paste-demo',
-            disabled=True
-        ),
-
-        html.Img(
-            id='image-paste-output'
         )
     ],
     style={
         'padding': 25
     }
-)
-
-app.clientside_callback(
-    '''(isHovering) => !isHovering;''',
-    Output('image-paste-demo', 'disabled'),
-    Input('image-paste-container', 'isHovering')
-)
-
-
-app.clientside_callback(
-    '''(imageInfo) => imageInfo.base64;''',
-    Output('image-paste-output', 'src'),
-    Input('image-paste-demo', 'imageInfo'),
-    prevent_initial_call=True
 )
 
 
