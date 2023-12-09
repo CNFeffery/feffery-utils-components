e22eb67c737dd7507e94abe4f80577f5ba6f2ebf
insistence <3055204202@qq.com>
2023-12-09 21:32:00 +0800

    doc: 重构FefferySetTitle参数注释

--- usage.py
+++ usage.py
@@ -1,6 +1,6 @@
 
 import dash
-from dash import html
+from dash import html, dcc
 import feffery_utils_components as fuc
 from dash.dependencies import Input, Output
 import uuid
@@ -9,24 +9,32 @@ app = dash.Dash(__name__, compress=True)
 
 app.layout = html.Div(
     [
-        fuc.FefferyScrollbars(
-            html.Div(
-                '请将鼠标移入本区域进行滚动\n'*100,
-                style={
-                    'whiteSpace': 'pre'
-                }
-            ),
+        dcc.Input(
+            id='set-title-demo',
+            placeholder='请输入新title信息',
             style={
-                'maxHeight': '150px',
-                'maxWidth': '200px',
-                'border': '1px dashed #e1dfdd'
+                'maxWidth': '200px'
             }
+        ),
+
+        fuc.FefferySetTitle(
+            id='set-title-demo-output'
         )
     ],
     style={
         'padding': 25
     }
 )
+
+
+@app.callback(
+    Output('set-title-demo-output', 'title'),
+    Input('set-title-demo', 'value'),
+    prevent_initial_call=True
+)
+def set_title_demo(value):
+
+    return value or dash.no_update
 
 
 if __name__ == '__main__':
