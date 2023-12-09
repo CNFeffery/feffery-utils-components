b3f9f4366f75c7c9e210336ec47502fbb93ee26a
insistence <3055204202@qq.com>
2023-12-09 21:43:06 +0800

    doc: 重构FefferyVirtualList参数注释

--- feffery_utils_components/FefferyVirtualList.py
+++ feffery_utils_components/FefferyVirtualList.py
@@ -9,15 +9,23 @@ class FefferyVirtualList(Component):
 
 Keyword arguments:
 
-- children (a list of or a singular dash component, string or number; optional)
+- children (a list of or a singular dash component, string or number; optional):
+    组件子元素.
 
-- id (string; optional)
+- id (string; optional):
+    组件id.
 
-- className (string; optional)
+- className (string; optional):
+    设置css类名.
 
-- height (number; required)
+- height (number; required):
+    虚拟化区域像素高度.
 
-- itemHeight (number; required)
+- itemHeight (number; required):
+    每个子元素区域的像素高度.
+
+- key (string; optional):
+    辅助刷新用唯一标识key值.
 
 - loading_state (dict; optional)
 
@@ -32,16 +40,17 @@ Keyword arguments:
     - prop_name (string; optional):
         Holds which property is loading.
 
-- style (dict; optional)"""
+- style (dict; optional):
+    设置css样式."""
     _children_props = []
     _base_nodes = ['children']
     _namespace = 'feffery_utils_components'
     _type = 'FefferyVirtualList'
     @_explicitize_args
-    def __init__(self, children=None, id=Component.UNDEFINED, style=Component.UNDEFINED, className=Component.UNDEFINED, height=Component.REQUIRED, itemHeight=Component.REQUIRED, loading_state=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['children', 'id', 'className', 'height', 'itemHeight', 'loading_state', 'style']
+    def __init__(self, children=None, id=Component.UNDEFINED, style=Component.UNDEFINED, className=Component.UNDEFINED, key=Component.UNDEFINED, height=Component.REQUIRED, itemHeight=Component.REQUIRED, loading_state=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['children', 'id', 'className', 'height', 'itemHeight', 'key', 'loading_state', 'style']
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['children', 'id', 'className', 'height', 'itemHeight', 'loading_state', 'style']
+        self.available_properties = ['children', 'id', 'className', 'height', 'itemHeight', 'key', 'loading_state', 'style']
         self.available_wildcard_properties =            []
         _explicit_args = kwargs.pop('_explicit_args')
         _locals = locals()
--- usage.py
+++ usage.py
@@ -1,64 +1,23 @@
-
-from datetime import datetime
-import time
 import dash
-from dash import html, dcc
+from dash import html
 import feffery_utils_components as fuc
 from dash.dependencies import Input, Output, State
-import uuid
 
 app = dash.Dash(__name__, compress=True)
 
 app.layout = html.Div(
     [
-
-        html.Div(
-            [
-                fuc.FefferyWheelColorPicker(
-                    id='top-progress-color',
-                    color='#e74c3c'
-                ),
-                fuc.FefferyFancyButton(
-                    '触发5秒耗时回调',
-                    id='top-progress-trigger-demo1',
-                    type='primary'
-                )
-            ]
-        ),
-        fuc.FefferyTopProgress(
-            html.Div(
-                id='top-progress-trigger-demo1-output'
-            ),
-            id='top-progress-demo',
-            listenPropsMode='exclude',
-            excludeProps=['top-progress-demo.color']
+        fuc.FefferyVirtualList(
+            id='virtual-list-demo',
+            height=400,
+            itemHeight=40,
+            children=[html.Div(i) for i in range(1000)]
         )
     ],
     style={
         'padding': 25
     }
 )
-
-
-@app.callback(
-    Output('top-progress-demo', 'color'),
-    Input('top-progress-color', 'color')
-)
-def top_progress_demo_update_color(color):
-
-    return color
-
-
-@app.callback(
-    Output('top-progress-trigger-demo1-output', 'children'),
-    Input('top-progress-trigger-demo1', 'nClicks'),
-    prevent_initial_call=True
-)
-def top_progress_demo(nClicks):
-
-    time.sleep(5)
-
-    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 
 if __name__ == '__main__':
