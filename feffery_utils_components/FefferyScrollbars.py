0b5b6e727a36c48147f7b4db47dca66911c91da9
insistence <3055204202@qq.com>
2023-12-09 21:30:10 +0800

    doc: 重构FefferyScrollbars参数注释

--- feffery_utils_components/FefferyScrollbars.py
+++ feffery_utils_components/FefferyScrollbars.py
@@ -9,27 +9,40 @@ class FefferyScrollbars(Component):
 
 Keyword arguments:
 
-- children (a list of or a singular dash component, string or number; optional)
+- children (a list of or a singular dash component, string or number; optional):
+    组件子元素.
 
-- id (string; optional)
+- id (string; optional):
+    组件id.
 
-- autoHide (boolean; default True)
+- autoHide (boolean; default True):
+    设置是否在无操作时自动隐藏滚动条，默认为True.
 
-- className (string; optional)
+- className (string; optional):
+    设置自定义css类名.
 
-- classNames (dict; optional)
+- classNames (dict; optional):
+    针对各组成部分单独设置自定义css类名.
 
     `classNames` is a dict with keys:
 
-    - content (string; optional)
+    - content (string; optional):
+        内容区域容器.
 
-    - scrollContent (string; optional)
+    - scrollContent (string; optional):
+        正在滚动的内容容器.
 
-    - scrollbar (string; optional)
+    - scrollbar (string; optional):
+        滚动条.
 
-    - track (string; optional)
+    - track (string; optional):
+        滚动条滑块.
 
-- forceVisible (boolean | a value equal to: 'x', 'y'; default False)
+- forceVisible (boolean | a value equal to: 'x', 'y'; default False):
+    设置滑块区域是否强制可见，默认为False.
+
+- key (string; optional):
+    辅助刷新用唯一标识key值.
 
 - loading_state (dict; optional)
 
@@ -44,22 +57,26 @@ Keyword arguments:
     - prop_name (string; optional):
         Holds which property is loading.
 
-- scrollbarMaxSize (number; optional)
+- scrollbarMaxSize (number; optional):
+    设置滚动条最大像素长度，默认无限制.
 
-- scrollbarMinSize (number; default 25)
+- scrollbarMinSize (number; default 25):
+    设置滚动条最小像素长度，默认为25.
 
-- style (dict; optional)
+- style (dict; optional):
+    设置自定义css样式.
 
-- timeout (number; default 1000)"""
+- timeout (number; default 1000):
+    设置滑块自动隐藏的毫秒数，默认为1000."""
     _children_props = []
     _base_nodes = ['children']
     _namespace = 'feffery_utils_components'
     _type = 'FefferyScrollbars'
     @_explicitize_args
-    def __init__(self, children=None, id=Component.UNDEFINED, style=Component.UNDEFINED, className=Component.UNDEFINED, autoHide=Component.UNDEFINED, classNames=Component.UNDEFINED, forceVisible=Component.UNDEFINED, timeout=Component.UNDEFINED, scrollbarMinSize=Component.UNDEFINED, scrollbarMaxSize=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['children', 'id', 'autoHide', 'className', 'classNames', 'forceVisible', 'loading_state', 'scrollbarMaxSize', 'scrollbarMinSize', 'style', 'timeout']
+    def __init__(self, children=None, id=Component.UNDEFINED, style=Component.UNDEFINED, className=Component.UNDEFINED, key=Component.UNDEFINED, autoHide=Component.UNDEFINED, classNames=Component.UNDEFINED, forceVisible=Component.UNDEFINED, timeout=Component.UNDEFINED, scrollbarMinSize=Component.UNDEFINED, scrollbarMaxSize=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['children', 'id', 'autoHide', 'className', 'classNames', 'forceVisible', 'key', 'loading_state', 'scrollbarMaxSize', 'scrollbarMinSize', 'style', 'timeout']
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['children', 'id', 'autoHide', 'className', 'classNames', 'forceVisible', 'loading_state', 'scrollbarMaxSize', 'scrollbarMinSize', 'style', 'timeout']
+        self.available_properties = ['children', 'id', 'autoHide', 'className', 'classNames', 'forceVisible', 'key', 'loading_state', 'scrollbarMaxSize', 'scrollbarMinSize', 'style', 'timeout']
         self.available_wildcard_properties =            []
         _explicit_args = kwargs.pop('_explicit_args')
         _locals = locals()
--- usage.py
+++ usage.py
@@ -9,104 +9,24 @@ app = dash.Dash(__name__, compress=True)
 
 app.layout = html.Div(
     [
-        html.Div(
-            [
-                fuc.FefferyFancyButton(
-                    '滚动至页面底部',
-                    id='scroll-to-bottom-demo',
-                    type='primary'
-                ),
-                fuc.FefferyFancyButton(
-                    '滚动至距离顶部800px处',
-                    id='scroll-top-offset-demo',
-                    type='primary'
-                ),
-                fuc.FefferyFancyButton(
-                    '相对当前位置向下滚动300px',
-                    id='scroll-relative-offset-demo',
-                    type='primary'
-                ),
-                fuc.FefferyFancyButton(
-                    '滚动至示例元素处',
-                    id='scroll-target-demo',
-                    type='primary'
-                )
-            ]
-        ),
-        html.Div(
-            [
-                html.Div(
-                    '示例目标元素',
-                    id='scroll-target-element'
-                ),
-                fuc.FefferyFancyButton(
-                    '滚动至页面顶部',
-                    id='scroll-to-top-demo',
-                    type='primary'
-                ),
-            ],
+        fuc.FefferyScrollbars(
+            html.Div(
+                '请将鼠标移入本区域进行滚动\n'*100,
+                style={
+                    'whiteSpace': 'pre'
+                }
+            ),
             style={
-                'padding': '1400px 0 800px 0',
-                'background': 'linear-gradient(180deg,#1890ff80,#fff)'
+                'maxHeight': '150px',
+                'maxWidth': '200px',
+                'border': '1px dashed #e1dfdd'
             }
-        ),
-
-        html.Div(
-            id='scroll-demo-output'
         )
     ],
     style={
         'padding': 25
     }
 )
-
-
-@app.callback(
-    Output('scroll-demo-output', 'children'),
-    [Input('scroll-to-top-demo', 'nClicks'),
-     Input('scroll-to-bottom-demo', 'nClicks'),
-     Input('scroll-top-offset-demo', 'nClicks'),
-     Input('scroll-relative-offset-demo', 'nClicks'),
-     Input('scroll-target-demo', 'nClicks')],
-    prevent_initial_call=True
-)
-def scroll_demo(*args):
-
-    # 基于dash的上下文功能获知当前回调由谁触发
-    trigger_id = dash.ctx.triggered_id
-
-    if trigger_id == 'scroll-to-top-demo':
-        return fuc.FefferyScroll(
-            executeScroll=True,
-            scrollMode='to-top'
-        )
-
-    elif trigger_id == 'scroll-to-bottom-demo':
-        return fuc.FefferyScroll(
-            executeScroll=True,
-            scrollMode='to-bottom'
-        )
-
-    elif trigger_id == 'scroll-top-offset-demo':
-        return fuc.FefferyScroll(
-            executeScroll=True,
-            scrollMode='top-offset',
-            scrollTopOffset=800
-        )
-
-    elif trigger_id == 'scroll-relative-offset-demo':
-        return fuc.FefferyScroll(
-            executeScroll=True,
-            scrollMode='relative-offset',
-            scrollRelativeOffset=300
-        )
-
-    elif trigger_id == 'scroll-target-demo':
-        return fuc.FefferyScroll(
-            executeScroll=True,
-            scrollMode='target',
-            scrollTargetId='scroll-target-element'
-        )
 
 
 if __name__ == '__main__':
