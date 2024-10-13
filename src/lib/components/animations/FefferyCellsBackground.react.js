4f9212f31ed35789e56448762b3a64deeeb7097f
insistence <3055204202@qq.com>
2024-10-13 18:33:34 +0800

    chore: FefferyCellsBackground整理源码及参数注释 #77

--- src/lib/components/animations/FefferyCellsBackground.react.js
+++ src/lib/components/animations/FefferyCellsBackground.react.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 
 const LazyFefferyCellsBackground = React.lazy(() => import(/* webpackChunkName: "feffery_animated_3d_background_three" */ '../../fragments/animations/FefferyCellsBackground.react'));
 
+/**
+ * 3D-Cells背景组件FefferyCellsBackground
+ */
 const FefferyCellsBackground = (props) => {
     return (
         <Suspense fallback={null}>
@@ -12,20 +15,29 @@ const FefferyCellsBackground = (props) => {
 }
 
 
-// 定义参数或属性
 FefferyCellsBackground.propTypes = {
     /**
-     * 组件id
+     * 组件唯一id
      */
     id: PropTypes.string,
 
     /**
-     * 设置内嵌元素内容
+     * 对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果
+     */
+    key: PropTypes.string,
+
+    /**
+     * 组件型，设置内嵌元素内容
      */
     children: PropTypes.node,
 
     /**
-     * css类名
+     * 当前组件css样式
+     */
+    style: PropTypes.object,
+
+    /**
+     * 当前组件css类名，支持[动态css](/advanced-classname)
      */
     className: PropTypes.oneOfType([
         PropTypes.string,
@@ -33,62 +45,62 @@ FefferyCellsBackground.propTypes = {
     ]),
 
     /**
-     * 自定义css字典
-     */
-    style: PropTypes.object,
-
-    /**
-     * 辅助刷新用唯一标识key值
-     */
-    key: PropTypes.string,
-
-    /**
-     * 设置是否开启鼠标控制，默认为true
+     * 设置是否开启鼠标控制
+     * 默认为`true`
      */
     mouseControls: PropTypes.bool,
 
     /**
-     * 设置是否开启触摸控制，默认为true
+     * 设置是否开启触摸控制
+     * 默认为`true`
      */
     touchControls: PropTypes.bool,
 
     /**
-     * 设置是否开启陀螺仪控制，默认为false
+     * 设置是否开启陀螺仪控制
+     * 默认为`false`
      */
     gyroControls: PropTypes.bool,
 
     /**
-     * 设置最小高度，默认为200.00
+     * 设置最小高度
+     * 默认为`200.00`
      */
     minHeight: PropTypes.number,
 
     /**
-     * 设置最小宽度，默认为200.00
+     * 设置最小宽度
+     * 默认为`200.00`
      */
     minWidth: PropTypes.number,
 
     /**
-     * 设置比例，默认为1.00
+     * 设置比例
+     * 默认为`1.00`
      */
     scale: PropTypes.number,
 
     /**
-     * 设置cells颜色1，默认为'#109090'
+     * 设置cells颜色1
+     * 默认为`'#109090'`
      */
     color1: PropTypes.string,
 
     /**
-     * 设置cells颜色2，默认为'#f2e735'
+     * 设置cells颜色2
+     * 默认为`'#f2e735'`
      */
     color2: PropTypes.string,
 
     /**
-     * 设置cells大小，范围0.2到5，默认为1.5
+     * 设置cells大小，范围`0.2`到`5`
+     * 默认为`1.5`
      */
     size: PropTypes.number,
 
     /**
-     * 设置cells动画速度，范围0到5，默认为1
+     * 设置cells动画速度，范围`0`到`5`
+     * 默认为`1`
      */
     speed: PropTypes.number,
 
