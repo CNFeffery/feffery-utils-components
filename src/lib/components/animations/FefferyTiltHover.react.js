9c9fd1a6c966456f3e3c61064f9903ca6159f7d2
insistence <3055204202@qq.com>
2024-10-13 18:36:31 +0800

    chore: FefferyTiltHover整理源码及参数注释 #77

--- src/lib/components/animations/FefferyTiltHover.react.js
+++ src/lib/components/animations/FefferyTiltHover.react.js
@@ -4,7 +4,9 @@ import useCss from '../../hooks/useCss'
 import { isString } from 'lodash';
 import PropTypes from 'prop-types';
 
-// 定义倾斜悬浮组件FefferyTiltHover，api参数参考：https://github.com/mkosir/react-parallax-tilt?tab=readme-ov-file#props
+/**
+ * 倾斜悬浮组件FefferyTiltHover
+ */
 const FefferyTiltHover = (props) => {
     // 取得必要属性或参数
     const {
@@ -101,150 +103,174 @@ const FefferyTiltHover = (props) => {
     );
 }
 
-// 定义参数或属性
+
 FefferyTiltHover.propTypes = {
     /**
-     * 组件id
+     * 组件唯一id
      */
     id: PropTypes.string,
 
     /**
-     * 要进行倾斜悬浮效果编排的目标元素
-     */
-    children: PropTypes.node,
-
-    /**
-     * css类名
-     */
-    className: PropTypes.string,
-
-    /**
-     * 自定义css字典
-     */
-    style: PropTypes.object,
-
-    /**
-     * 辅助刷新用唯一标识key值
+     * 对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果
      */
     key: PropTypes.string,
 
     /**
-     * 设置是否启用倾斜效果，默认为true
+     * 组件型，要进行倾斜悬浮效果编排的目标元素
+     */
+    children: PropTypes.node,
+
+    /**
+     * 当前组件css样式
+     */
+    style: PropTypes.object,
+
+    /**
+     * 当前组件css类名，支持[动态css](/advanced-classname)
+     */
+    className: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.object
+    ]),
+
+    /**
+     * 设置是否启用倾斜效果
+     * 默认为`true`
      */
     tiltEnable: PropTypes.bool,
 
     /**
-     * 设置是否反转倾斜方向，默认为false
+     * 设置是否反转倾斜方向
+     * 默认为`false`
      */
     tiltReverse: PropTypes.bool,
 
     /**
-     * 设置x轴上的初始倾斜值（度），默认为0
+     * 设置x轴上的初始倾斜值（度）
+     * 默认为`0`
      */
     tiltAngleXInitial: PropTypes.number,
 
     /**
-     * 设置y轴上的初始倾斜值（度），默认为0
+     * 设置y轴上的初始倾斜值（度）
+     * 默认为`0`
      */
     tiltAngleYInitial: PropTypes.number,
 
     /**
-     * 设置x轴上的的最大倾斜旋转（度），范围为0到90，默认为20
+     * 设置x轴上的的最大倾斜旋转（度），范围为`0`到`90`
+     * 默认为`20`
      */
     tiltMaxAngleX: PropTypes.number,
 
     /**
-     * 设置y轴上的最大倾斜旋转（度），范围为0到90，默认为20
+     * 设置y轴上的最大倾斜旋转（度），范围为`0`到`90`
+     * 默认为`20`
      */
     tiltMaxAngleY: PropTypes.number,
 
     /**
-     * 启用单轴倾斜，可选值为'x'或'y'，默认为undefined
+     * 启用单轴倾斜，可选值为`'x'`或`'y'`
      */
     tiltAxis: PropTypes.oneOf(['x', 'y']),
 
     /**
-     * 设置在x轴上手动倾斜旋转（度），默认为null
+     * 设置在x轴上手动倾斜旋转（度）
      */
     tiltAngleXManual: PropTypes.number,
 
     /**
-     * 设置在y轴上手动倾斜旋转（度），默认为null
+     * 设置在y轴上手动倾斜旋转（度）
      */
     tiltAngleYManual: PropTypes.number,
 
     /**
-     * 设置是否启用眩光效果，默认为false
+     * 设置是否启用眩光效果
+     * 默认为`false`
      */
     glareEnable: PropTypes.bool,
 
     /**
-     * 设置最大眩光不透明度，范围0到1，默认为0.7
+     * 设置最大眩光不透明度，范围`0`到`1`
+     * 默认为`0.7`
      */
     glareMaxOpacity: PropTypes.number,
 
     /**
-     * 设置眩光效果的颜色，默认为'#ffffff'
+     * 设置眩光效果的颜色
+     * 默认为`'#ffffff'`
      */
     glareColor: PropTypes.string,
 
     /**
-     * 设置眩光效果的边框半径，接受任何标准的CSS边框半径。如果眩光颜色与页面颜色不同，则很有用，默认为'0'
+     * 设置眩光效果的边框半径，接受任何标准的`CSS`边框半径。如果眩光颜色与页面颜色不同，则很有用
+     * 默认为`'0'`
      */
     glareBorderRadius: PropTypes.string,
 
     /**
-     * 设置眩光效果的位置，可选的值为'top', 'right', 'bottom', 'left', 'all'，默认为'bottom'
+     * 设置眩光效果的位置，可选的值为`'top'`、`'right'`、`'bottom'`、`'left'`、`'all'`
+     * 默认为`'bottom'`
      */
     glarePosition: PropTypes.oneOf(['top', 'right', 'bottom', 'left', 'all']),
 
     /**
-     * 设置是否反转眩光方向，默认为false
+     * 设置是否反转眩光方向
+     * 默认为`false`
      */
     glareReverse: PropTypes.bool,
 
     /**
-     * 设置组件的比例（1.5 = 150%，2 = 200%，等），默认为1
+     * 设置组件的比例（`1.5 = 150%`，`2 = 200%`，等）
+     * 默认为`1`
      */
     scale: PropTypes.number,
 
     /**
-     * 定义对象（包装/子组件）与用户的距离。越低，倾斜度越大，默认为1000
+     * 定义对象（包装/子组件）与用户的距离。越低，倾斜度越大
+     * 默认为`1000`
      */
     perspective: PropTypes.number,
 
     /**
-     * 设置是否启用组件的垂直翻转，默认为false
+     * 设置是否启用组件的垂直翻转
+     * 默认为`false`
      */
     flipVertically: PropTypes.bool,
 
     /**
-     * 设置是否启用组件的水平翻转，默认为false
+     * 设置是否启用组件的水平翻转
+     * 默认为`false`
      */
     flipHorizontally: PropTypes.bool,
 
     /**
-     * 设置是否必须在事件中重置效果，默认为true
+     * 设置是否必须在事件中重置效果
+     * 默认为`true`
      */
     reset: PropTypes.bool,
 
     /**
-     * 设置在操作组件时过渡效果，默认为'cubic-bezier(.03,.98,.52,.99)'
+     * 设置在操作组件时过渡效果
+     * 默认为`'cubic-bezier(.03,.98,.52,.99)'`
      */
     transitionEasing: PropTypes.string,
 
     /**
-     * 设置在操作组件时的过渡速度，默认为400
+     * 设置在操作组件时的过渡速度
+     * 默认为`400`
      */
     transitionSpeed: PropTypes.number,
 
     /**
-     * 设置是否跟踪整个窗口上的鼠标和触摸事件，默认为false
+     * 设置是否跟踪整个窗口上的鼠标和触摸事件
+     * 默认为`false`
      */
     trackOnWindow: PropTypes.bool,
 
     /**
-     * 设置是否启用设备方向检测，默认为false
+     * 设置是否启用设备方向检测
+     * 默认为`false`
      */
     gyroscope: PropTypes.bool,
 
