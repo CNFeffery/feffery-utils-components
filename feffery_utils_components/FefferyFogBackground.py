f46b3e12f37b2da0c21e031d11aa6381436591af
insistence <3055204202@qq.com>
2024-10-13 18:34:34 +0800

    chore: FefferyFogBackground整理源码及参数注释 #77

--- feffery_utils_components/FefferyFogBackground.py
+++ feffery_utils_components/FefferyFogBackground.py
@@ -5,60 +5,60 @@ from dash.development.base_component import Component, _explicitize_args
 
 class FefferyFogBackground(Component):
     """A FefferyFogBackground component.
-
+3D-Fog背景组件FefferyFogBackground
 
 Keyword arguments:
 
 - id (string; optional):
-    组件id.
-
-- children (a list of or a singular dash component, string or number; optional):
-    设置内嵌元素内容.
-
-- className (string | dict; optional):
-    css类名.
-
-- style (dict; optional):
-    自定义css字典.
+    组件唯一id.
 
 - key (string; optional):
-    辅助刷新用唯一标识key值.
+    对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果.
+
+- children (a list of or a singular dash component, string or number; optional):
+    组件型，设置内嵌元素内容.
+
+- style (dict; optional):
+    当前组件css样式.
+
+- className (string | dict; optional):
+    当前组件css类名，支持[动态css](/advanced-classname).
 
 - mouseControls (boolean; default True):
-    设置是否开启鼠标控制，默认为True.
+    设置是否开启鼠标控制  `默认为True`.
 
 - touchControls (boolean; default True):
-    设置是否开启触摸控制，默认为True.
+    设置是否开启触摸控制  默认为`True`.
 
 - gyroControls (boolean; default False):
-    设置是否开启陀螺仪控制，默认为False.
+    设置是否开启陀螺仪控制  默认为`False`.
 
 - minHeight (number; default 200.00):
-    设置最小高度，默认为200.00.
+    设置最小高度  默认为`200.00`.
 
 - minWidth (number; default 200.00):
-    设置最小宽度，默认为200.00.
+    设置最小宽度  默认为`200.00`.
 
 - highlightColor (string; default '#ffc300'):
-    设置高亮颜色，默认为'#ffc300'.
+    设置高亮颜色  默认为`'#ffc300'`.
 
 - midtoneColor (string; default '#ff1f00'):
-    设置中间调颜色，默认为'#ff1f00'.
+    设置中间调颜色  默认为`'#ff1f00'`.
 
 - lowlightColor (string; default '#2d00ff'):
-    设置低亮颜色，默认为'#2d00ff'.
+    设置低亮颜色  默认为`'#2d00ff'`.
 
 - baseColor (string; default '#ffebeb'):
-    设置基本颜色，默认为'#ffebeb'.
+    设置基本颜色  默认为`'#ffebeb'`.
 
 - blurFactor (number; default 0.6):
-    设置模糊因子，范围0.1到0.9，默认为0.6.
+    设置模糊因子，范围`0.1`到`0.9`  默认为`0.6`.
 
 - zoom (number; default 1):
-    设置缩放大小，范围0.1到3，默认为1.
+    设置缩放大小，范围`0.1`到`3`  默认为`1`.
 
 - speed (number; default 1):
-    设置动画速度，范围0到5，默认为1.
+    设置动画速度，范围`0`到`5`  默认为`1`.
 
 - loading_state (dict; optional)
 
@@ -77,10 +77,10 @@ Keyword arguments:
     _namespace = 'feffery_utils_components'
     _type = 'FefferyFogBackground'
     @_explicitize_args
-    def __init__(self, children=None, id=Component.UNDEFINED, className=Component.UNDEFINED, style=Component.UNDEFINED, key=Component.UNDEFINED, mouseControls=Component.UNDEFINED, touchControls=Component.UNDEFINED, gyroControls=Component.UNDEFINED, minHeight=Component.UNDEFINED, minWidth=Component.UNDEFINED, highlightColor=Component.UNDEFINED, midtoneColor=Component.UNDEFINED, lowlightColor=Component.UNDEFINED, baseColor=Component.UNDEFINED, blurFactor=Component.UNDEFINED, zoom=Component.UNDEFINED, speed=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['id', 'children', 'className', 'style', 'key', 'mouseControls', 'touchControls', 'gyroControls', 'minHeight', 'minWidth', 'highlightColor', 'midtoneColor', 'lowlightColor', 'baseColor', 'blurFactor', 'zoom', 'speed', 'loading_state']
+    def __init__(self, children=None, id=Component.UNDEFINED, key=Component.UNDEFINED, style=Component.UNDEFINED, className=Component.UNDEFINED, mouseControls=Component.UNDEFINED, touchControls=Component.UNDEFINED, gyroControls=Component.UNDEFINED, minHeight=Component.UNDEFINED, minWidth=Component.UNDEFINED, highlightColor=Component.UNDEFINED, midtoneColor=Component.UNDEFINED, lowlightColor=Component.UNDEFINED, baseColor=Component.UNDEFINED, blurFactor=Component.UNDEFINED, zoom=Component.UNDEFINED, speed=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'key', 'children', 'style', 'className', 'mouseControls', 'touchControls', 'gyroControls', 'minHeight', 'minWidth', 'highlightColor', 'midtoneColor', 'lowlightColor', 'baseColor', 'blurFactor', 'zoom', 'speed', 'loading_state']
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['id', 'children', 'className', 'style', 'key', 'mouseControls', 'touchControls', 'gyroControls', 'minHeight', 'minWidth', 'highlightColor', 'midtoneColor', 'lowlightColor', 'baseColor', 'blurFactor', 'zoom', 'speed', 'loading_state']
+        self.available_properties = ['id', 'key', 'children', 'style', 'className', 'mouseControls', 'touchControls', 'gyroControls', 'minHeight', 'minWidth', 'highlightColor', 'midtoneColor', 'lowlightColor', 'baseColor', 'blurFactor', 'zoom', 'speed', 'loading_state']
         self.available_wildcard_properties =            []
         _explicit_args = kwargs.pop('_explicit_args')
         _locals = locals()
--- src/lib/components/animations/FefferyFogBackground.react.js
+++ src/lib/components/animations/FefferyFogBackground.react.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 
 const LazyFefferyFogBackground = React.lazy(() => import(/* webpackChunkName: "feffery_animated_3d_background_three" */ '../../fragments/animations/FefferyFogBackground.react'));
 
+/**
+ * 3D-Fog背景组件FefferyFogBackground
+ */
 const FefferyFogBackground = (props) => {
     return (
         <Suspense fallback={null}>
@@ -12,20 +15,29 @@ const FefferyFogBackground = (props) => {
 }
 
 
-// 定义参数或属性
 FefferyFogBackground.propTypes = {
     /**
-     * 组件id
+     * 组件唯一id
      */
     id: PropTypes.string,
 
     /**
-     * 设置内嵌元素内容
+     * 对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果
+     */
+    key: PropTypes.string,
+
+    /**
+     * 组件型，设置内嵌元素内容
      */
     children: PropTypes.node,
 
     /**
-     * css类名
+     * 当前组件css样式
+     */
+    style: PropTypes.object,
+
+    /**
+     * 当前组件css类名，支持[动态css](/advanced-classname)
      */
     className: PropTypes.oneOfType([
         PropTypes.string,
@@ -33,72 +45,74 @@ FefferyFogBackground.propTypes = {
     ]),
 
     /**
-     * 自定义css字典
-     */
-    style: PropTypes.object,
-
-    /**
-     * 辅助刷新用唯一标识key值
-     */
-    key: PropTypes.string,
-
-    /**
-     * 设置是否开启鼠标控制，默认为true
+     * 设置是否开启鼠标控制
+     * `默认为true`
      */
     mouseControls: PropTypes.bool,
 
     /**
-     * 设置是否开启触摸控制，默认为true
+     * 设置是否开启触摸控制
+     * 默认为`true`
      */
     touchControls: PropTypes.bool,
 
     /**
-     * 设置是否开启陀螺仪控制，默认为false
+     * 设置是否开启陀螺仪控制
+     * 默认为`false`
      */
     gyroControls: PropTypes.bool,
 
     /**
-     * 设置最小高度，默认为200.00
+     * 设置最小高度
+     * 默认为`200.00`
      */
     minHeight: PropTypes.number,
 
     /**
-     * 设置最小宽度，默认为200.00
+     * 设置最小宽度
+     * 默认为`200.00`
      */
     minWidth: PropTypes.number,
 
     /**
-     * 设置高亮颜色，默认为'#ffc300'
+     * 设置高亮颜色
+     * 默认为`'#ffc300'`
      */
     highlightColor: PropTypes.string,
 
     /**
-     * 设置中间调颜色，默认为'#ff1f00'
+     * 设置中间调颜色
+     * 默认为`'#ff1f00'`
      */
     midtoneColor: PropTypes.string,
 
     /**
-     * 设置低亮颜色，默认为'#2d00ff'
+     * 设置低亮颜色
+     * 默认为`'#2d00ff'`
      */
     lowlightColor: PropTypes.string,
 
     /**
-     * 设置基本颜色，默认为'#ffebeb'
+     * 设置基本颜色
+     * 默认为`'#ffebeb'`
      */
     baseColor: PropTypes.string,
 
     /**
-     * 设置模糊因子，范围0.1到0.9，默认为0.6
+     * 设置模糊因子，范围`0.1`到`0.9`
+     * 默认为`0.6`
      */
     blurFactor: PropTypes.number,
 
     /**
-     * 设置缩放大小，范围0.1到3，默认为1
+     * 设置缩放大小，范围`0.1`到`3`
+     * 默认为`1`
      */
     zoom: PropTypes.number,
 
     /**
-     * 设置动画速度，范围0到5，默认为1
+     * 设置动画速度，范围`0`到`5`
+     * 默认为`1`
      */
     speed: PropTypes.number,
 
