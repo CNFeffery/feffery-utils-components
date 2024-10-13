eadda263746d0443bfd641b3ef56750e112f4df3
insistence <3055204202@qq.com>
2024-10-13 18:37:23 +0800

    chore: FefferyWavesBackground整理源码及参数注释 #77

--- feffery_utils_components/FefferyWavesBackground.py
+++ feffery_utils_components/FefferyWavesBackground.py
@@ -5,60 +5,60 @@ from dash.development.base_component import Component, _explicitize_args
 
 class FefferyWavesBackground(Component):
     """A FefferyWavesBackground component.
-
+3D-Waves背景组件FefferyWavesBackground
 
 Keyword arguments:
 
 - id (string; optional):
-    组件id.
-
-- children (a list of or a singular dash component, string or number; optional):
-    设置内嵌元素内容.
-
-- className (string | dict; optional):
-    css类名.
-
-- style (dict; optional):
-    自定义css字典.
+    组件唯一id.
 
 - key (string; optional):
-    辅助刷新用唯一标识key值.
+    对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果.
+
+- children (a list of or a singular dash component, string or number; optional):
+    组件型，设置内嵌元素内容.
+
+- style (dict; optional):
+    当前组件css样式.
+
+- className (string | dict; optional):
+    当前组件css类名，支持[动态css](/advanced-classname).
 
 - mouseControls (boolean; default True):
-    设置是否开启鼠标控制，默认为True.
+    设置是否开启鼠标控制  默认为`True`.
 
 - touchControls (boolean; default True):
-    设置是否开启触摸控制，默认为True.
+    设置是否开启触摸控制  默认为`True`.
 
 - gyroControls (boolean; default False):
-    设置是否开启陀螺仪控制，默认为False.
+    设置是否开启陀螺仪控制  默认为`False`.
 
 - minHeight (number; default 200.00):
-    设置最小高度，默认为200.00.
+    设置最小高度  默认为`200.00`.
 
 - minWidth (number; default 200.00):
-    设置最小宽度，默认为200.00.
+    设置最小宽度  默认为`200.00`.
 
 - scale (number; default 1.00):
-    设置比例，默认为1.00.
+    设置比例  默认为`1.00`.
 
 - scaleMobile (number; default 1.00):
-    设置移动端比例，默认为1.00.
+    设置移动端比例  默认为`1.00`.
 
 - color (string; default '#11619a'):
-    设置wave颜色，默认为'#11619a'.
+    设置wave颜色  默认为`'#11619a'`.
 
 - shininess (number; default 30):
-    设置光泽度，范围0到150，默认为30.
+    设置光泽度，范围`0`到`150`  默认为`30`.
 
 - waveHeight (number; default 15):
-    设置wave高度，范围0到40，默认为15.
+    设置wave高度，范围`0`到`40`  默认为`15`.
 
 - waveSpeed (number; default 1):
-    设置wave速度，范围0到2，默认为1.
+    设置wave速度，范围`0`到`2`  默认为`1`.
 
 - zoom (number; default 1):
-    设置缩放大小，范围0.7到1.8，默认为1.
+    设置缩放大小，范围`0.7`到`1.8`  默认为`1`.
 
 - loading_state (dict; optional)
 
@@ -77,10 +77,10 @@ Keyword arguments:
     _namespace = 'feffery_utils_components'
     _type = 'FefferyWavesBackground'
     @_explicitize_args
-    def __init__(self, children=None, id=Component.UNDEFINED, className=Component.UNDEFINED, style=Component.UNDEFINED, key=Component.UNDEFINED, mouseControls=Component.UNDEFINED, touchControls=Component.UNDEFINED, gyroControls=Component.UNDEFINED, minHeight=Component.UNDEFINED, minWidth=Component.UNDEFINED, scale=Component.UNDEFINED, scaleMobile=Component.UNDEFINED, color=Component.UNDEFINED, shininess=Component.UNDEFINED, waveHeight=Component.UNDEFINED, waveSpeed=Component.UNDEFINED, zoom=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['id', 'children', 'className', 'style', 'key', 'mouseControls', 'touchControls', 'gyroControls', 'minHeight', 'minWidth', 'scale', 'scaleMobile', 'color', 'shininess', 'waveHeight', 'waveSpeed', 'zoom', 'loading_state']
+    def __init__(self, children=None, id=Component.UNDEFINED, key=Component.UNDEFINED, style=Component.UNDEFINED, className=Component.UNDEFINED, mouseControls=Component.UNDEFINED, touchControls=Component.UNDEFINED, gyroControls=Component.UNDEFINED, minHeight=Component.UNDEFINED, minWidth=Component.UNDEFINED, scale=Component.UNDEFINED, scaleMobile=Component.UNDEFINED, color=Component.UNDEFINED, shininess=Component.UNDEFINED, waveHeight=Component.UNDEFINED, waveSpeed=Component.UNDEFINED, zoom=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'key', 'children', 'style', 'className', 'mouseControls', 'touchControls', 'gyroControls', 'minHeight', 'minWidth', 'scale', 'scaleMobile', 'color', 'shininess', 'waveHeight', 'waveSpeed', 'zoom', 'loading_state']
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['id', 'children', 'className', 'style', 'key', 'mouseControls', 'touchControls', 'gyroControls', 'minHeight', 'minWidth', 'scale', 'scaleMobile', 'color', 'shininess', 'waveHeight', 'waveSpeed', 'zoom', 'loading_state']
+        self.available_properties = ['id', 'key', 'children', 'style', 'className', 'mouseControls', 'touchControls', 'gyroControls', 'minHeight', 'minWidth', 'scale', 'scaleMobile', 'color', 'shininess', 'waveHeight', 'waveSpeed', 'zoom', 'loading_state']
         self.available_wildcard_properties =            []
         _explicit_args = kwargs.pop('_explicit_args')
         _locals = locals()
