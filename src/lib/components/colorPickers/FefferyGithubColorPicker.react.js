9fa163519096ca22e6fc05e3ac50ff163fce7677
CNFeffery <fefferypzy@gmail.com>
2024-10-15 15:20:33 +0800

    chore: FefferyGithubColorPicker整理源码及参数注释 #77

--- src/lib/components/colorPickers/FefferyGithubColorPicker.react.js
+++ src/lib/components/colorPickers/FefferyGithubColorPicker.react.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 
 const LazyFefferyGithubColorPicker = React.lazy(() => import(/* webpackChunkName: "feffery_color_pickers" */ '../../fragments/colorPickers/FefferyGithubColorPicker.react'));
 
+/**
+ * Github风格色彩选择器FefferyGithubColorPicker
+ */
 const FefferyGithubColorPicker = (props) => {
     return (
         <Suspense fallback={null}>
@@ -11,37 +14,41 @@ const FefferyGithubColorPicker = (props) => {
     );
 }
 
-// 定义参数或属性
 FefferyGithubColorPicker.propTypes = {
     /**
-     * 组件id
+     * 组件唯一id
      */
     id: PropTypes.string,
 
     /**
-     * css样式
+     * 对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果
+     */
+    key: PropTypes.string,
+
+    /**
+     * 当前组件css样式
      */
     style: PropTypes.object,
 
     /**
-     * css类名
+     * 当前组件css类名
      */
     className: PropTypes.string,
 
     /**
-     * 设置或监听当前选中色彩对应16进制颜色值
+     * 监听或设置当前选中色彩对应16进制颜色值
      */
     color: PropTypes.string,
 
     /**
      * 设置可选色彩对应16进制颜色值数组
-     * 默认：['#B80000', '#DB3E00', '#FCCB00', '#008B02', '#006B76', '#1273DE', '#004DCF', '#5300EB', '#EB9694', '#FAD0C3', '#FEF3BD', '#C1E1C5', '#BEDADC', '#C4DEF6', '#BED3F3', '#D4C4FB']
+     * 默认值：`['#B80000', '#DB3E00', '#FCCB00', '#008B02', '#006B76', '#1273DE', '#004DCF', '#5300EB', '#EB9694', '#FAD0C3', '#FEF3BD', '#C1E1C5', '#BEDADC', '#C4DEF6', '#BED3F3', '#D4C4FB']`
      */
     colors: PropTypes.arrayOf(PropTypes.string),
 
     /**
-     * 设置顶部箭头的方位，可选的有'hide'、'top-left'、'top-right'
-     * 默认：'top-left'
+     * 顶部箭头方位，可选项有`'hide'`、`'top-left'`、`'top-right'`
+     * 默认值：`'top-left'`
      */
     triangle: PropTypes.oneOf(['hide', 'top-left', 'top-right']),
 
@@ -61,7 +68,6 @@ FefferyGithubColorPicker.propTypes = {
     })
 };
 
-// 设置默认参数
 FefferyGithubColorPicker.defaultProps = {
     triangle: 'top-left',
     colors: ['#B80000', '#DB3E00', '#FCCB00', '#008B02', '#006B76', '#1273DE', '#004DCF', '#5300EB', '#EB9694', '#FAD0C3', '#FEF3BD', '#C1E1C5', '#BEDADC', '#C4DEF6', '#BED3F3', '#D4C4FB']
