b224c5c1aea1d3179211338cf6a17791d4d123d2
CNFeffery <fefferypzy@gmail.com>
2024-10-18 22:44:13 +0800

    chore: FefferySortable整理源码及参数注释 #77

--- src/lib/components/draggable/FefferySortable.react.js
+++ src/lib/components/draggable/FefferySortable.react.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 
 const LazyFefferySortable = React.lazy(() => import(/* webpackChunkName: "feffery_sortable" */ '../../fragments/draggable/FefferySortable.react'));
 
+/**
+ * 排序列表组件FefferySortable
+ */
 const FefferySortable = (props) => {
     return (
         <Suspense fallback={null}>
@@ -11,15 +14,19 @@ const FefferySortable = (props) => {
     );
 }
 
-// 定义参数或属性
 FefferySortable.propTypes = {
     /**
-     * 组件id
+     * 组件唯一id
      */
     id: PropTypes.string,
 
     /**
-     * 组件css样式
+     * 对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果
+     */
+    key: PropTypes.string,
+
+    /**
+     * 当前组件css样式
      */
     style: PropTypes.object,
 
@@ -29,7 +36,7 @@ FefferySortable.propTypes = {
     handleStyle: PropTypes.object,
 
     /**
-     * 拖拽手柄css类
+     * 拖拽手柄css类名
      */
     handleClassName: PropTypes.oneOfType([
         PropTypes.string,
@@ -37,7 +44,7 @@ FefferySortable.propTypes = {
     ]),
 
     /**
-     * 组件css类
+     * 当前组件css类名
      */
     className: PropTypes.oneOfType([
         PropTypes.string,
@@ -45,17 +52,12 @@ FefferySortable.propTypes = {
     ]),
 
     /**
-     * 强制刷新用
-     */
-    key: PropTypes.string,
-
-    /**
-     * 必填参数，用于定义子项源数组，数组顺序不会受拖拽排序结果影响
+     * 必填，定义子项源数组，数组顺序不会受拖拽排序结果影响
      */
     items: PropTypes.arrayOf(
         PropTypes.exact({
             /**
-             * 对应当前子元素的唯一标识
+             * 对应当前子元素唯一标识
              */
             key: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
 
@@ -65,12 +67,12 @@ FefferySortable.propTypes = {
             content: PropTypes.node,
 
             /**
-             * 当前子元素容器css样式
+             * 当前子元素容器`css`样式
              */
             style: PropTypes.object,
 
             /**
-             * 当前子元素容器css类
+             * 当前子元素容器`css`类名
              */
             className: PropTypes.oneOfType([
                 PropTypes.string,
@@ -78,12 +80,12 @@ FefferySortable.propTypes = {
             ]),
 
             /**
-             * 当前子元素处于拖拽中状态下的css样式
+             * 当前子元素处于拖拽中状态下的`css`样式
              */
             draggingStyle: PropTypes.object,
 
             /**
-             * 当前子元素处于拖拽中状态下的css类
+             * 当前子元素处于拖拽中状态下的`css`类名
              */
             draggingClassName: PropTypes.oneOfType([
                 PropTypes.string,
@@ -93,33 +95,36 @@ FefferySortable.propTypes = {
     ).isRequired,
 
     /**
-     * 设置排序列表的方向，可选的有'vertical'和'horizontal'
-     * 默认：'vertical'
+     * 排序列表方向，可选项有`'vertical'`、`'horizontal'`
+     * 默认值：`'vertical'`
      */
     direction: PropTypes.oneOf(['vertical', 'horizontal']),
 
     /**
-     * 设置子项处于拖拽中状态下的缩放比例，默认为1即不缩放
+     * 设置子项处于拖拽中状态下的缩放比例
+     * 默认值：`1`
      */
     itemDraggingScale: PropTypes.number,
 
     /**
-     * 设置拖拽手柄位置，默认为'end'
+     * 设置拖拽手柄位置，可选项有`'start'`、`'end'`
+     * 默认值：`'end'`
      */
     handlePosition: PropTypes.oneOf(['start', 'end']),
 
     /**
-     * 设置内置的推拽手柄图标类型，默认为'holder'
+     * 设置内置的推拽手柄图标类型，可选项有`'holder'`、`'menu'`、`'drag'`
+     * 默认值：`'holder'`
      */
     handleType: PropTypes.oneOf(['holder', 'menu', 'drag']),
 
     /**
-     * 限制横向拖拽最大像素偏移距离，默认无限制
+     * 限制横向拖拽最大像素偏移距离
      */
     maxTranslateX: PropTypes.number,
 
     /**
-     * 限制纵向拖拽最大像素偏移距离，默认无限制
+     * 限制纵向拖拽最大像素偏移距离
      */
     maxTranslateY: PropTypes.number,
 
@@ -133,18 +138,11 @@ FefferySortable.propTypes = {
         ])
     ),
 
-    // 子项点选相关功能
     /**
-     * 设置或监听当前处于选中状态的子项key值
+     * 设置或监听当前处于选中状态的子项`key`值
      */
     value: PropTypes.oneOfType([
-        /**
-         * 单选模式
-         */
         PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-        /**
-         * 多选模式
-         */
         PropTypes.arrayOf(
             PropTypes.oneOfType([PropTypes.number, PropTypes.string])
         )
@@ -152,18 +150,18 @@ FefferySortable.propTypes = {
 
     /**
      * 是否允许多选
-     * 默认：false
+     * 默认值：`false`
      */
     multiple: PropTypes.bool,
 
     /**
      * 是否允许无选中项
-     * 默认：true
+     * 默认值：`true`
      */
     allowNoValue: PropTypes.bool,
 
     /**
-     * 针对已选中项设置额外的css样式
+     * 针对已选中项设置额外的`css`样式
      */
     selectedStyle: PropTypes.object,
 
@@ -189,7 +187,6 @@ FefferySortable.propTypes = {
     setProps: PropTypes.func,
 };
 
-// 设置默认参数
 FefferySortable.defaultProps = {
     direction: 'vertical',
     itemDraggingScale: 1,
