80d9daadc609bfbd9b45a48242326a5bf55261c2
CNFeffery <fefferypzy@gmail.com>
2024-10-18 22:36:09 +0800

    chore: FefferyRND整理源码及参数注释 #77

--- feffery_utils_components/FefferyRND.py
+++ feffery_utils_components/FefferyRND.py
@@ -5,27 +5,27 @@ from dash.development.base_component import Component, _explicitize_args
 
 class FefferyRND(Component):
     """A FefferyRND component.
-
+尺寸可调可拖拽组件FefferyRND
 
 Keyword arguments:
 
 - id (string; optional):
-    组件id.
+    组件唯一id.
 
 - key (string; optional):
-    强制刷新用.
-
-- style (dict; optional):
-    css样式.
-
-- className (string; optional):
-    css类名.
+    对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果.
 
 - children (a list of or a singular dash component, string or number; optional):
-    内部元素.
+    组件型，内嵌元素.
+
+- style (dict; optional):
+    当前组件css样式.
+
+- className (string; optional):
+    当前组件css类名.
 
 - defaultState (dict; optional):
-    设置默认位置及尺寸信息.
+    设置默认位置及尺寸状态.
 
     `defaultState` is a dict with keys:
 
@@ -36,24 +36,24 @@ Keyword arguments:
         竖直方向像素位置.
 
     - width (number | string; optional):
-        宽度值，数值型表示像素宽度，字符型与css相关属性值格式兼容，如'300px'、'50%'等.
+        宽度值，数值型表示像素宽度，字符型与`css`相关属性值格式兼容，如'300px'、'50%'等.
 
     - height (number | string; optional):
-        高度值，数值型表示像素高度，字符型与css相关属性值格式兼容，如'300px'、'50%'等.
+        高度值，数值型表示像素高度，字符型与`css`相关属性值格式兼容，如'300px'、'50%'等.
 
 - size (dict; optional):
-    设置或监听当前组件尺寸信息.
+    设置或监听当前组件尺寸状态.
 
     `size` is a dict with keys:
 
     - width (number | string; optional):
-        宽度值，数值型表示像素宽度，字符型与css相关属性值格式兼容，如'300px'、'50%'等.
+        宽度值，数值型表示像素宽度，字符型与`css`相关属性值格式兼容，如'300px'、'50%'等.
 
     - height (number | string; optional):
-        高度值，数值型表示像素高度，字符型与css相关属性值格式兼容，如'300px'、'50%'等.
+        高度值，数值型表示像素高度，字符型与`css`相关属性值格式兼容，如'300px'、'50%'等.
 
 - position (dict; optional):
-    设置或监听当前组件位置信息.
+    设置或监听当前组件位置状态.
 
     `position` is a dict with keys:
 
@@ -64,54 +64,54 @@ Keyword arguments:
         竖直方向像素位置.
 
 - minWidth (number | string; optional):
-    限制可调宽度下限，数值型表示像素宽度，字符型与css相关属性值格式兼容，如'300px'、'50%'等.
+    限制可调宽度下限，数值型表示像素宽度，字符型与`css`相关属性值格式兼容，如'300px'、'50%'等.
 
 - minHeight (number | string; optional):
-    限制可调高度下限，数值型表示像素宽度，字符型与css相关属性值格式兼容，如'300px'、'50%'等.
+    限制可调高度下限，数值型表示像素宽度，字符型与`css`相关属性值格式兼容，如'300px'、'50%'等.
 
 - maxWidth (number | string; optional):
-    限制可调宽度上限，数值型表示像素宽度，字符型与css相关属性值格式兼容，如'300px'、'50%'等.
+    限制可调宽度上限，数值型表示像素宽度，字符型与`css`相关属性值格式兼容，如'300px'、'50%'等.
 
 - maxHeight (number | string; optional):
-    限制可调高度上限，数值型表示像素宽度，字符型与css相关属性值格式兼容，如'300px'、'50%'等.
+    限制可调高度上限，数值型表示像素宽度，字符型与`css`相关属性值格式兼容，如'300px'、'50%'等.
 
 - resizeGrid (list of numbers; optional):
-    针对尺寸调整行为，设置水平和竖直方向上调整的像素步长，格式为：[水平像素步长, 竖直像素步长]  默认：[1, 1].
+    针对尺寸调整行为，设置水平和竖直方向上调整的像素步长，格式：`[水平像素步长, 竖直像素步长]`  默认值：`[1, 1]`.
 
 - dragGrid (list of numbers; optional):
-    针对位置拖拽行为，设置水平和竖直方向上拖拽的像素步长，格式为：[水平像素步长, 竖直像素步长]  默认：[1, 1].
+    针对位置拖拽行为，设置水平和竖直方向上拖拽的像素步长，格式：`[水平像素步长, 竖直像素步长]`  默认值：`[1, 1]`.
 
 - lockAspectRatio (boolean | number; optional):
-    配置高宽比锁定  传入True时，将锁定初始尺寸对应高宽比  传入数值型时，用于指定具体的高宽比  默认：False.
+    配置高宽比锁定，传入`True`时，将锁定初始尺寸对应高宽比，传入数值型时，用于指定具体的高宽比  默认值：`False`.
 
 - lockAspectRatioExtraWidth (number; optional):
-    在lockAspectRatio基础上，设置额外像素宽度  默认：0.
+    在`lockAspectRatio`基础上，设置额外像素宽度  默认值：`0`.
 
 - lockAspectRatioExtraHeight (number; optional):
-    在lockAspectRatio基础上，设置额外像素高度  默认：0.
+    在`lockAspectRatio`基础上，设置额外像素高度  默认值：`0`.
 
 - direction (list of a value equal to: 'top', 'right', 'bottom', 'left', 'topRight', 'bottomRight', 'bottomLeft', 'topLeft's; default ['top', 'right', 'bottom', 'left', 'topRight', 'bottomRight', 'bottomLeft', 'topLeft']):
-    设置允许进行尺寸调整的方向，多选，可选项有'top'、'right'、'bottom'、'left'、'topRight'、'bottomRight'、'bottomLeft'、'topLeft'
-    默认为['top', 'right', 'bottom', 'left', 'topRight', 'bottomRight',
-    'bottomLeft', 'topLeft'].
+    允许进行尺寸调整的方向，可多选，可选项有`'top'`、`'right'`、`'bottom'`、`'left'`、`'topRight'`、`'bottomRight'`、`'bottomLeft'`、`'topLeft'`
+    默认值：`['top', 'right', 'bottom', 'left', 'topRight', 'bottomRight',
+    'bottomLeft', 'topLeft']`.
 
 - disableDragging (boolean; optional):
     是否禁用拖拽功能.
 
 - dragAxis (a value equal to: 'x', 'y', 'both', 'none'; optional):
-    允许进行拖拽的方向，可选的有'x'、'y'、'both'、'none'.
+    允许进行拖拽的方向，可选项有`'x'`、`'y'`、`'both'`、`'none'`.
 
 - bounds (a value equal to: 'window', 'parent'; optional):
-    设置当前组件尺寸调整及拖拽的外边界类型，可选的有'window'、'parent'  默认为'window'.
+    设置当前组件尺寸调整及拖拽的外边界类型，可选项有`'window'`、`'parent'`  默认值：`'window'`.
 
 - selected (boolean; default False):
-    设置或监听当前组件是否处于选择状态  默认：False.
+    设置或监听当前组件是否处于选择状态  默认值：`False`.
 
 - selectedStyle (dict; optional):
-    设置当前组件在选中状态下的css样式.
+    设置当前组件在选中状态下的`css`样式.
 
 - selectedClassName (string; optional):
-    配置当前组件在选中状态下的css类名.
+    配置当前组件在选中状态下的`css`类名.
 
 - loading_state (dict; optional)
 
@@ -131,9 +131,9 @@ Keyword arguments:
     _type = 'FefferyRND'
     @_explicitize_args
     def __init__(self, children=None, id=Component.UNDEFINED, key=Component.UNDEFINED, style=Component.UNDEFINED, className=Component.UNDEFINED, defaultState=Component.UNDEFINED, size=Component.UNDEFINED, position=Component.UNDEFINED, minWidth=Component.UNDEFINED, minHeight=Component.UNDEFINED, maxWidth=Component.UNDEFINED, maxHeight=Component.UNDEFINED, resizeGrid=Component.UNDEFINED, dragGrid=Component.UNDEFINED, lockAspectRatio=Component.UNDEFINED, lockAspectRatioExtraWidth=Component.UNDEFINED, lockAspectRatioExtraHeight=Component.UNDEFINED, direction=Component.UNDEFINED, disableDragging=Component.UNDEFINED, dragAxis=Component.UNDEFINED, bounds=Component.UNDEFINED, selected=Component.UNDEFINED, selectedStyle=Component.UNDEFINED, selectedClassName=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['id', 'key', 'style', 'className', 'children', 'defaultState', 'size', 'position', 'minWidth', 'minHeight', 'maxWidth', 'maxHeight', 'resizeGrid', 'dragGrid', 'lockAspectRatio', 'lockAspectRatioExtraWidth', 'lockAspectRatioExtraHeight', 'direction', 'disableDragging', 'dragAxis', 'bounds', 'selected', 'selectedStyle', 'selectedClassName', 'loading_state']
+        self._prop_names = ['id', 'key', 'children', 'style', 'className', 'defaultState', 'size', 'position', 'minWidth', 'minHeight', 'maxWidth', 'maxHeight', 'resizeGrid', 'dragGrid', 'lockAspectRatio', 'lockAspectRatioExtraWidth', 'lockAspectRatioExtraHeight', 'direction', 'disableDragging', 'dragAxis', 'bounds', 'selected', 'selectedStyle', 'selectedClassName', 'loading_state']
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['id', 'key', 'style', 'className', 'children', 'defaultState', 'size', 'position', 'minWidth', 'minHeight', 'maxWidth', 'maxHeight', 'resizeGrid', 'dragGrid', 'lockAspectRatio', 'lockAspectRatioExtraWidth', 'lockAspectRatioExtraHeight', 'direction', 'disableDragging', 'dragAxis', 'bounds', 'selected', 'selectedStyle', 'selectedClassName', 'loading_state']
+        self.available_properties = ['id', 'key', 'children', 'style', 'className', 'defaultState', 'size', 'position', 'minWidth', 'minHeight', 'maxWidth', 'maxHeight', 'resizeGrid', 'dragGrid', 'lockAspectRatio', 'lockAspectRatioExtraWidth', 'lockAspectRatioExtraHeight', 'direction', 'disableDragging', 'dragAxis', 'bounds', 'selected', 'selectedStyle', 'selectedClassName', 'loading_state']
         self.available_wildcard_properties =            []
         _explicit_args = kwargs.pop('_explicit_args')
         _locals = locals()
