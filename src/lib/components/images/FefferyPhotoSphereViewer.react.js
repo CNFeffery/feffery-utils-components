737aa53e55c8f1ad40800e4ddf4d9f5a54b3bd8e
CNFeffery <fefferypzy@gmail.com>
2024-10-20 18:03:45 +0800

    chore: FefferyPhotoSphereViewer整理源码及参数注释 #77

--- src/lib/components/images/FefferyPhotoSphereViewer.react.js
+++ src/lib/components/images/FefferyPhotoSphereViewer.react.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 
 const LazyFefferyPhotoSphereViewer = React.lazy(() => import(/* webpackChunkName: "feffery_photo_sphere_viewer" */ '../../fragments/images/FefferyPhotoSphereViewer.react'));
 
+/**
+ * 全景图片查看器组件FefferyPhotoSphereViewer
+ */
 const FefferyPhotoSphereViewer = (props) => {
     return (
         <Suspense fallback={null}>
@@ -11,47 +14,45 @@ const FefferyPhotoSphereViewer = (props) => {
     );
 }
 
-// 定义参数或属性
 FefferyPhotoSphereViewer.propTypes = {
     /**
-     * 组件id
+     * 组件唯一id
      */
     id: PropTypes.string,
 
     /**
-     * 辅助刷新用唯一标识key值
+     * 对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果
      */
     key: PropTypes.string,
 
     /**
-     * 设置全景图片资源地址
+     * 全景图片资源地址
      */
     src: PropTypes.string,
 
     /**
-     * 设置查看器宽度，同css中的width属性
+     * 设置查看器宽度，同`css`中的`width`属性
      */
     width: PropTypes.string,
 
     /**
-     * 设置查看器高度，同css中的height属性
+     * 设置查看器高度，同`css`中的`height`属性
      */
     height: PropTypes.string,
 
     /**
      * 是否开启小星球模式
-     * 默认：false
+     * 默认值：`false`
      */
     littlePlanet: PropTypes.bool,
 
     /**
-     * 设置查看器所在容器css类名
+     * 设置查看器所在容器`css`类名
      */
     containerClass: PropTypes.string,
 
     /**
-     * 配置导航栏中需要显示的功能项及顺序，设置为`false`时将隐藏导航栏
-     * 可选的有`'zoom'`、`'move'`、`'download'`、`'caption'`、`'fullscreen'`、`'autorotate'`
+     * 配置导航栏中需要显示的功能项及顺序，设置为`false`时将隐藏导航栏，可选项有`'zoom'`、`'move'`、`'download'`、`'caption'`、`'fullscreen'`、`'autorotate'`
      * 默认值：`['caption']`
      */
     navbar: PropTypes.oneOfType(
@@ -64,7 +65,7 @@ FefferyPhotoSphereViewer.propTypes = {
     ),
 
     /**
-     * 导航栏标题内容，支持HTML字符串，仅当navbar中包含'caption'时有效
+     * 导航栏标题内容，支持`HTML`字符串，仅当`navbar`中包含`'caption'`时有效
      */
     caption: PropTypes.string,
 
@@ -85,31 +86,31 @@ FefferyPhotoSphereViewer.propTypes = {
 
     /**
      * 是否允许鼠标滚轮缩放
-     * 默认：true
+     * 默认值：`true`
      */
     mousewheel: PropTypes.bool,
 
     /**
      * 是否允许鼠标拖拽平移
-     * 默认：true
+     * 默认值：`true`
      */
     mousemove: PropTypes.bool,
 
     /**
      * 设置鼠标平移速度
-     * 默认：1
+     * 默认值：`1`
      */
     moveSpeed: PropTypes.number,
 
     /**
      * 设置鼠标滚轮缩放速度
-     * 默认：1
+     * 默认值：`1`
      */
     zoomSpeed: PropTypes.number,
 
     /**
      * 是否开启鱼眼模式
-     * 默认：false
+     * 默认值：`false`
      */
     fisheye: PropTypes.bool,
 
@@ -218,7 +219,6 @@ FefferyPhotoSphereViewer.propTypes = {
     })
 };
 
-// 设置默认参数
 FefferyPhotoSphereViewer.defaultProps = {
     littlePlanet: false,
     navbar: ['caption'],
