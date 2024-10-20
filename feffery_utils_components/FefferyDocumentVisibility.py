09a7eca84178c384a86bd8a1489f8dd8d965da82
CNFeffery <fefferypzy@gmail.com>
2024-10-20 19:17:32 +0800

    chore: FefferyDocumentVisibility整理源码及参数注释 #77

--- feffery_utils_components/FefferyDocumentVisibility.py
+++ feffery_utils_components/FefferyDocumentVisibility.py
@@ -5,15 +5,18 @@ from dash.development.base_component import Component, _explicitize_args
 
 class FefferyDocumentVisibility(Component):
     """A FefferyDocumentVisibility component.
-
+页面可见性检查组件FefferyDocumentVisibility
 
 Keyword arguments:
 
 - id (string; optional):
-    组件id.
+    组件唯一id.
+
+- key (string; optional):
+    对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果.
 
 - documentVisibility (a value equal to: 'visible', 'hidden'; optional):
-    监听页面是否可见.
+    监听页面是否可见，可选项有`'visible'`、`'hidden'`.
 
 - loading_state (dict; optional)
 
@@ -32,10 +35,10 @@ Keyword arguments:
     _namespace = 'feffery_utils_components'
     _type = 'FefferyDocumentVisibility'
     @_explicitize_args
-    def __init__(self, id=Component.UNDEFINED, documentVisibility=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['id', 'documentVisibility', 'loading_state']
+    def __init__(self, id=Component.UNDEFINED, key=Component.UNDEFINED, documentVisibility=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'key', 'documentVisibility', 'loading_state']
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['id', 'documentVisibility', 'loading_state']
+        self.available_properties = ['id', 'key', 'documentVisibility', 'loading_state']
         self.available_wildcard_properties =            []
         _explicit_args = kwargs.pop('_explicit_args')
         _locals = locals()
