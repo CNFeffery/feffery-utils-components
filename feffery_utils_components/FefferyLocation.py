784399404b89103215abef8512a6f01d50e9918c
CNFeffery <fefferypzy@gmail.com>
2024-10-21 11:17:14 +0800

    chore: FefferyLocation整理源码及参数注释 #77

--- feffery_utils_components/FefferyLocation.py
+++ feffery_utils_components/FefferyLocation.py
@@ -5,39 +5,42 @@ from dash.development.base_component import Component, _explicitize_args
 
 class FefferyLocation(Component):
     """A FefferyLocation component.
-
+地址监听组件FefferyLocation
 
 Keyword arguments:
 
 - id (string; optional):
-    组件id.
+    组件唯一id.
+
+- key (string; optional):
+    对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果.
 
 - href (string; optional):
-    监听最新的href信息.
+    监听最新的`href`信息.
 
 - pathname (string; optional):
-    监听最新的pathname信息.
+    监听最新的`pathname`信息.
 
 - search (string; optional):
-    监听最新的search信息.
+    监听最新的`search`信息.
 
 - hash (string; optional):
-    监听最新的hash信息.
+    监听最新的`hash`信息.
 
 - host (string; optional):
-    监听最新的host信息.
+    监听最新的`host`信息.
 
 - hostname (string; optional):
-    监听最新的hostname信息.
+    监听最新的`hostname`信息.
 
 - port (string; optional):
-    监听最新的port信息.
+    监听最新的`port`信息.
 
 - protocol (string; optional):
-    监听最新的protocol信息.
+    监听最新的`protocol`信息.
 
 - trigger (a value equal to: 'load', 'pushstate', 'popstate'; optional):
-    监听最近一次地址更新行为触发类型，'load'表示页面重载行为，'pushstate'表示动态更新行为，'popstate'表示返回上一步地址.
+    监听最近一次地址更新行为触发类型，`'load'`表示页面重载行为，`'pushstate'`表示动态更新行为，`'popstate'`表示返回上一步地址.
 
 - loading_state (dict; optional)
 
@@ -56,10 +59,10 @@ Keyword arguments:
     _namespace = 'feffery_utils_components'
     _type = 'FefferyLocation'
     @_explicitize_args
-    def __init__(self, id=Component.UNDEFINED, href=Component.UNDEFINED, pathname=Component.UNDEFINED, search=Component.UNDEFINED, hash=Component.UNDEFINED, host=Component.UNDEFINED, hostname=Component.UNDEFINED, port=Component.UNDEFINED, protocol=Component.UNDEFINED, trigger=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['id', 'href', 'pathname', 'search', 'hash', 'host', 'hostname', 'port', 'protocol', 'trigger', 'loading_state']
+    def __init__(self, id=Component.UNDEFINED, key=Component.UNDEFINED, href=Component.UNDEFINED, pathname=Component.UNDEFINED, search=Component.UNDEFINED, hash=Component.UNDEFINED, host=Component.UNDEFINED, hostname=Component.UNDEFINED, port=Component.UNDEFINED, protocol=Component.UNDEFINED, trigger=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'key', 'href', 'pathname', 'search', 'hash', 'host', 'hostname', 'port', 'protocol', 'trigger', 'loading_state']
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['id', 'href', 'pathname', 'search', 'hash', 'host', 'hostname', 'port', 'protocol', 'trigger', 'loading_state']
+        self.available_properties = ['id', 'key', 'href', 'pathname', 'search', 'hash', 'host', 'hostname', 'port', 'protocol', 'trigger', 'loading_state']
         self.available_wildcard_properties =            []
         _explicit_args = kwargs.pop('_explicit_args')
         _locals = locals()
