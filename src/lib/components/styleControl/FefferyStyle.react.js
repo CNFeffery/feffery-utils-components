f0d78d84535b1d92e11f7a40eda126f0a5cdda6e
insistence <3055204202@qq.com>
2024-10-23 22:45:00 +0800

    chore: FefferyStyle整理源码及参数注释 #77

--- src/lib/components/styleControl/FefferyStyle.react.js
+++ src/lib/components/styleControl/FefferyStyle.react.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 
-// 定义动态样式组件FefferyStyle
+/**
+ * 动态样式组件FefferyStyle
+ */
 const FefferyStyle = (props) => {
 
     const {
@@ -20,20 +22,19 @@ const FefferyStyle = (props) => {
         } >{rawStyle}</style>);
 }
 
-// 定义参数或属性
 FefferyStyle.propTypes = {
     /**
-     * 组件id
+     * 组件唯一id
      */
     id: PropTypes.string,
 
     /**
-     * 辅助刷新用唯一标识key值
+     * 对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果
      */
     key: PropTypes.string,
 
     /**
-     * 设置要添加到文档中的原生css字符
+     * 设置要添加到文档中的原生`css`字符
      */
     rawStyle: PropTypes.string,
 
