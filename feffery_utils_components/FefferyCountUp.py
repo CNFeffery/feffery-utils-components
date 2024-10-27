f7cdd96d32b2e50d2d9d30f1cc4ec0b27dd74520
CNFeffery <fefferypzy@gmail.com>
2024-10-27 21:57:37 +0800

    chore: FefferyCountUp整理源码及参数注释 #77

--- feffery_utils_components/FefferyCountUp.py
+++ feffery_utils_components/FefferyCountUp.py
@@ -5,45 +5,45 @@ from dash.development.base_component import Component, _explicitize_args
 
 class FefferyCountUp(Component):
     """A FefferyCountUp component.
-
+数字递增组件FefferyCountUp
 
 Keyword arguments:
 
 - id (string; optional):
-    组件id.
-
-- className (string; optional):
-    css类名.
-
-- style (dict; optional):
-    自定义css字典.
+    组件唯一id.
 
 - key (string; optional):
-    强制刷新用.
+    对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果.
+
+- style (dict; optional):
+    当前组件css样式.
+
+- className (string; optional):
+    当前组件css类名.
 
 - end (number; required):
-    设置数字递增目标，必填  对此参数的更新会重新触发递增动画.
+    必填，数字递增目标值，每次更新此参数后会重新触发递增动画.
 
 - start (number; default 0):
-    设置数字递增起点，默认为0  对此参数的更新会重新触发递增动画.
+    数字递增起始值，每次更新此参数后会重新触发递增动画  默认值：`0`.
 
 - duration (number; default 2):
-    设置数字递增动画耗时，单位：秒，默认为2  对此参数的更新会重新触发递增动画.
+    数字递增动画耗时，单位：秒，每次更新此参数后会重新触发递增动画  默认值：`2`.
 
 - decimals (number; default 0):
-    设置小数精度，默认为0.
+    小数精度  默认值：`0`.
 
 - enableScrollSpy (boolean; default True):
-    设置是否在当前元素进入视口后才开始执行递增动画，默认为True.
+    是否在当前元素进入视口后才开始执行递增动画  默认值：`True`.
 
 - scrollSpyDelay (number; default 0):
-    当enableScrollSpy为True时，设置当前元素进入视口后延时多久开始执行递增动画，单位：毫秒，默认为0.
+    当`enableScrollSpy=True`时，设置当前元素进入视口后延时多久开始执行递增动画，单位：毫秒  默认值：`0`.
 
 - scrollSpyOnce (boolean; default True):
-    设置是否仅进行一次视口出现后启用动画行为，默认为True.
+    是否仅进行一次视口出现后启用动画行为  默认值：`True`.
 
 - separator (string; default ','):
-    设置自定义千分符，默认为','.
+    自定义千分符  默认值：`','`.
 
 - loading_state (dict; optional)
 
@@ -62,10 +62,10 @@ Keyword arguments:
     _namespace = 'feffery_utils_components'
     _type = 'FefferyCountUp'
     @_explicitize_args
-    def __init__(self, id=Component.UNDEFINED, className=Component.UNDEFINED, style=Component.UNDEFINED, key=Component.UNDEFINED, end=Component.REQUIRED, start=Component.UNDEFINED, duration=Component.UNDEFINED, decimals=Component.UNDEFINED, enableScrollSpy=Component.UNDEFINED, scrollSpyDelay=Component.UNDEFINED, scrollSpyOnce=Component.UNDEFINED, separator=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['id', 'className', 'style', 'key', 'end', 'start', 'duration', 'decimals', 'enableScrollSpy', 'scrollSpyDelay', 'scrollSpyOnce', 'separator', 'loading_state']
+    def __init__(self, id=Component.UNDEFINED, key=Component.UNDEFINED, style=Component.UNDEFINED, className=Component.UNDEFINED, end=Component.REQUIRED, start=Component.UNDEFINED, duration=Component.UNDEFINED, decimals=Component.UNDEFINED, enableScrollSpy=Component.UNDEFINED, scrollSpyDelay=Component.UNDEFINED, scrollSpyOnce=Component.UNDEFINED, separator=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'key', 'style', 'className', 'end', 'start', 'duration', 'decimals', 'enableScrollSpy', 'scrollSpyDelay', 'scrollSpyOnce', 'separator', 'loading_state']
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['id', 'className', 'style', 'key', 'end', 'start', 'duration', 'decimals', 'enableScrollSpy', 'scrollSpyDelay', 'scrollSpyOnce', 'separator', 'loading_state']
+        self.available_properties = ['id', 'key', 'style', 'className', 'end', 'start', 'duration', 'decimals', 'enableScrollSpy', 'scrollSpyDelay', 'scrollSpyOnce', 'separator', 'loading_state']
         self.available_wildcard_properties =            []
         _explicit_args = kwargs.pop('_explicit_args')
         _locals = locals()
