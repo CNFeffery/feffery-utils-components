0a6dcb454f2a6f00731810fefd2d6cbef948209e
insistence <3055204202@qq.com>
2024-10-27 13:41:24 +0800

    chore: FefferyFancyMessage整理源码及参数注释 #77

--- feffery_utils_components/FefferyFancyMessage.py
+++ feffery_utils_components/FefferyFancyMessage.py
@@ -5,48 +5,49 @@ from dash.development.base_component import Component, _explicitize_args
 
 class FefferyFancyMessage(Component):
     """A FefferyFancyMessage component.
-
+美观消息提示组件FefferyFancyMessage
 
 Keyword arguments:
 
-- children (a list of or a singular dash component, string or number; optional):
-    组件子元素.
-
 - id (string; optional):
-    组件id.
+    组件唯一id.
 
 - key (string; optional):
-    辅助刷新用唯一标识key值.
+    对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果.
 
-- className (string; optional):
-    设置容器的css类名.
+- children (a list of or a singular dash component, string or number; optional):
+    组件型，设置内嵌元素内容.
 
 - style (dict; optional):
-    设置容器的css样式.
+    当前组件css样式.
+
+- className (string | dict; optional):
+    当前组件css类名，支持[动态css](/advanced-classname).
 
 - visible (boolean; default True):
-    主动设置是否可见，默认为False.
+    主动设置是否可见  默认值：`False`.
 
 - position (a value equal to: 'top-left', 'top-center', 'top-right', 'bottom-left', 'bottom-center', 'bottom-right'; default 'top-center'):
-    设置消息提示的弹出方位，可选的有'top-left'、'top-center'、'top-right'、'bottom-left'、'bottom-center'、'bottom-right'，默认为'top-center'.
+    设置消息提示的弹出方位，可选的有`'top-left'`、`'top-center'`、`'top-right'`、`'bottom-left'`、`'bottom-center'`、`'bottom-right'`
+    默认值：`'top-center'`.
 
 - reverseOrder (boolean; default True):
-    设置较新的消息提示是否从底部进行追加，默认为True.
+    设置较新的消息提示是否从底部进行追加  默认值：`True`.
 
 - containerClassName (string; optional):
-    设置容器的css类名.
+    设置容器的`css`类名.
 
 - containerStyle (dict; optional):
-    设置容器的css样式.
+    设置容器的`css`样式.
 
 - gutter (number; default 8):
-    设置相邻消息提示之间的像素间距，默认为8.
+    设置相邻消息提示之间的像素间距  默认值：`8`.
 
 - type (a value equal to: 'blank', 'success', 'error'; default 'blank'):
-    设置信息类型，可选的有'blank', 'success', 'error'  默认为'blank'.
+    设置信息类型，可选的有`'blank'`、`'success'`、`'error'`  默认值：`'blank'`.
 
 - duration (number; default 4000):
-    设置消息提示显示时长（单位：毫秒），默认为4000.
+    设置消息提示显示时长（单位：毫秒）  默认值：`4000`.
 
 - icon (a list of or a singular dash component, string or number; optional):
     自定义消息提示图标.
@@ -68,10 +69,10 @@ Keyword arguments:
     _namespace = 'feffery_utils_components'
     _type = 'FefferyFancyMessage'
     @_explicitize_args
-    def __init__(self, children=None, id=Component.UNDEFINED, key=Component.UNDEFINED, className=Component.UNDEFINED, style=Component.UNDEFINED, visible=Component.UNDEFINED, position=Component.UNDEFINED, reverseOrder=Component.UNDEFINED, containerClassName=Component.UNDEFINED, containerStyle=Component.UNDEFINED, gutter=Component.UNDEFINED, type=Component.UNDEFINED, duration=Component.UNDEFINED, icon=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['children', 'id', 'key', 'className', 'style', 'visible', 'position', 'reverseOrder', 'containerClassName', 'containerStyle', 'gutter', 'type', 'duration', 'icon', 'loading_state']
+    def __init__(self, children=None, id=Component.UNDEFINED, key=Component.UNDEFINED, style=Component.UNDEFINED, className=Component.UNDEFINED, visible=Component.UNDEFINED, position=Component.UNDEFINED, reverseOrder=Component.UNDEFINED, containerClassName=Component.UNDEFINED, containerStyle=Component.UNDEFINED, gutter=Component.UNDEFINED, type=Component.UNDEFINED, duration=Component.UNDEFINED, icon=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'key', 'children', 'style', 'className', 'visible', 'position', 'reverseOrder', 'containerClassName', 'containerStyle', 'gutter', 'type', 'duration', 'icon', 'loading_state']
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['children', 'id', 'key', 'className', 'style', 'visible', 'position', 'reverseOrder', 'containerClassName', 'containerStyle', 'gutter', 'type', 'duration', 'icon', 'loading_state']
+        self.available_properties = ['id', 'key', 'children', 'style', 'className', 'visible', 'position', 'reverseOrder', 'containerClassName', 'containerStyle', 'gutter', 'type', 'duration', 'icon', 'loading_state']
         self.available_wildcard_properties =            []
         _explicit_args = kwargs.pop('_explicit_args')
         _locals = locals()
