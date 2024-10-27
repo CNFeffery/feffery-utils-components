3f1526ad95314d5c52158dfbbef7e47f291c08d4
insistence <3055204202@qq.com>
2024-10-27 13:42:14 +0800

    chore: FefferyCaptcha整理源码及参数注释 #77

--- src/lib/components/verification/FefferyCaptcha.react.js
+++ src/lib/components/verification/FefferyCaptcha.react.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 
 const LazyFefferyCaptcha = React.lazy(() => import(/* webpackChunkName: "feffery_captcha" */ '../../fragments/verification/FefferyCaptcha.react'));
 
+/**
+ * 验证码组件FefferyCaptcha
+ */
 const FefferyCaptcha = (props) => {
     return (
         <Suspense fallback={null}>
@@ -11,22 +14,29 @@ const FefferyCaptcha = (props) => {
     );
 }
 
-// 定义参数或属性
 FefferyCaptcha.propTypes = {
     /**
-     * 组件id
+     * 组件唯一id
      */
     id: PropTypes.string,
 
     /**
-     * css类名
+     * 对当前组件的`key`值进行更新，可实现强制重绘当前组件的效果
      */
-    className: PropTypes.string,
+    key: PropTypes.string,
 
     /**
-     * 自定义css字典
+     * 当前组件css样式
      */
     style: PropTypes.object,
+
+    /**
+     * 当前组件css类名，支持[动态css](/advanced-classname)
+     */
+    className: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.object
+    ]),
 
     /**
      * 对应当前的验证码字符串
@@ -39,27 +49,31 @@ FefferyCaptcha.propTypes = {
     charNum: PropTypes.number,
 
     /**
-     * 设置验证码的像素高度，默认为40
+     * 设置验证码的像素高度
+     * 默认值：`40`
      */
     height: PropTypes.number,
 
     /**
-     * 设置验证码的像素宽度，默认为100
+     * 设置验证码的像素宽度
+     * 默认值：`100`
      */
     width: PropTypes.number,
 
     /**
-     * 设置验证码图片背景色，默认为'#DFF0D8'
+     * 设置验证码图片背景色
+     * 默认值：`'#DFF0D8'`
      */
     bgColor: PropTypes.string,
 
     /**
-     * 设置验证码字体像素大小，默认为25
+     * 设置验证码字体像素大小
+     * 默认值：`25`
      */
     fontSize: PropTypes.number,
 
     /**
-     * 用于手动刷新验证码，当传入true时会强制刷新验证码，再自动重置为false
+     * 用于手动刷新验证码，当传入`true`时会强制刷新验证码，再自动重置为`false`
      */
     refresh: PropTypes.bool,
 
