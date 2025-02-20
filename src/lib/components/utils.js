9cfcd42bae7923a9b4cc22e7b4d73dfab88c058d
CNFeffery <fefferypzy@gmail.com>
2025-02-20 15:28:28 +0800

    refactor: 重构_dashprivate_layout相关逻辑 #117

--- src/lib/components/utils.js
+++ src/lib/components/utils.js
@@ -7,25 +7,7 @@ const parseChildrenToArray = children => {
     return children;
 };
 
-
-const resolveChildProps = child => {
-    // This may need to change in the future if https://github.com/plotly/dash-renderer/issues/84 is addressed
-    if (
-        // disabled is a defaultProp (so it's always set)
-        // meaning that if it's not set on child.props, the actual
-        // props we want are lying a bit deeper - which means they
-        // are coming from Dash
-        isNil(child.props?.disabled) &&
-        child.props._dashprivate_layout &&
-        child.props._dashprivate_layout.props
-    ) {
-        // props are coming from Dash
-        return child.props._dashprivate_layout.props;
-    } else {
-        // else props are coming from React (e.g. Demo.js, or Tabs.test.js)
-        return child.props;
-    }
-};
+const resolveChildProps = child => window.dash_component_api.getLayout(child.props.componentPath)?.props;
 
 const useLoading = () => window.dash_component_api.useDashContext().useLoading() || undefined;
 
